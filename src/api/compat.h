d1e7139e3ec51dc165e2d01694a6d73292601046
Francesco Abbate <francesco.bbt@gmail.com>
2020-11-29 11:45:32 +0100

    Initial draft of transition to use LuaJIT2 instead of Lua 5.2
    There is problem with utf8 expression matching on common.utf8_chars

---
+++ src/api/compat.h
@@ -0,0 +1,17 @@
+#ifndef COMPAT_H
+#define COMPAT_H
+
+#include <lua.h>
+#include <lauxlib.h>
+
+#define luaL_newlibtable(L, l) \
+  (lua_createtable(L, 0, sizeof(l)/sizeof(*(l))-1))
+#define luaL_newlib(L, l) \
+  (luaL_newlibtable(L, l), luaL_register(L, NULL, l))
+
+extern void luaL_requiref (lua_State *L, const char *modname, lua_CFunction openf, int glb);
+extern void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup);
+extern void luaL_setmetatable (lua_State *L, const char *tname);
+extern int luaL_getsubtable (lua_State *L, int i, const char *name);
+
+#endif
