bd6778834831a1b01bcb2488e51c71bdb7ca1b69
daubaris <paulius.daubaris@gmail.com>
2021-03-06 09:41:41 +0200

    Enable customizing cursor speed
    Addresses issue #65

--- data/core/config.lua
+++ data/core/config.lua
@@ -19,6 +19,7 @@ config.line_limit = 80
 config.max_symbols = 4000
 config.max_project_files = 2000
 config.transitions = true
+config.blink_period = 0.8
 
 -- Disable plugin loading setting to false the config entry
 -- of the same name.
