c0c6bc5be9d412d34d263d6371384b3b3722c9fb
Adam Harrison <adamdharrison@gmail.com>
2021-05-04 18:31:46 -0400

    Replaced pattern-based commands with regexes.

--- data/core/doc/search.lua
+++ data/core/doc/search.lua
@@ -15,12 +15,8 @@ local function init_args(doc, line, col, text, opt)
   opt = opt or default_opt
   line, col = doc:sanitize_position(line, col)
 
-  if opt.no_case then
-    if opt.pattern then
-      text = text:gsub("%%?.", pattern_lower)
-    else
-      text = text:lower()
-    end
+  if opt.no_case and not opt.regex then
+    text = text:lower()
   end
 
   return doc, line, col, text, opt
@@ -30,20 +26,32 @@ end
 function search.find(doc, line, col, text, opt)
   doc, line, col, text, opt = init_args(doc, line, col, text, opt)
 
+  local re
+  if opt.regex then
+    re = regex.compile(text, opt.no_case and "i" or "")
+  end
   for line = line, #doc.lines do
     local line_text = doc.lines[line]
-    if opt.no_case then
-      line_text = line_text:lower()
+    if opt.regex then
+      local s, e = re:match(line_text, col, true)
+      if s then
+        return line, s, line, e + 1
+      end
+      col = 1
+    else
+      if opt.no_case then
+        line_text = line_text:lower()
+      end
+      local s, e = line_text:find(text, col, true)
+      if s then
+        return line, s, line, e + 1
+      end
+      col = 1
     end
-    local s, e = line_text:find(text, col, not opt.pattern)
-    if s then
-      return line, s, line, e + 1
-    end
-    col = 1
   end
 
   if opt.wrap then
-    opt = { no_case = opt.no_case, pattern = opt.pattern }
+    opt = { no_case = opt.no_case, regex = opt.regex }
     return search.find(doc, 1, 1, text, opt)
   end
 end
