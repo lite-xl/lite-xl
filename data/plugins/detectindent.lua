lite-xl/lite-xl

commit 6e08c3321c2770f6ecada953b9bb610fd64c58a9

Fixed a bug where if detectindent is paired with another plugin that hooks the event, it'll overwrite the other plugin's functions. (#190)

data/plugins/detectindent.lua
@@ -102,19 +102,26 @@ end
 local doc_on_text_change = Doc.on_text_change
 local adjust_threshold = 4
 
+local current_on_text_change = nil
+
 local function update_cache(doc)
   local type, size, score = detect_indent_stat(doc)
   cache[doc] = { type = type, size = size, confirmed = (score >= adjust_threshold) }
   doc.indent_info = cache[doc]
   if score < adjust_threshold and doc_on_text_change then
-    Doc.on_text_change = function(self, ...)
-      doc_on_text_change(self, ...)
+    current_on_text_change = function(self, ...)
       update_cache(self)
     end
   elseif score >= adjust_threshold and doc_on_text_change then
-    Doc.on_text_change = doc_on_text_change
-    doc_on_text_change = nil
+    current_on_text_change = nil
   end
+end
+
+function Doc:on_text_change(...)
+  if current_on_text_change then
+    current_on_text_change(...)
+  end
+  doc_on_text_change(...)
 end
 
 

commit 92322986b8e938a9d76c5420600db9e867871dec

Fix error with previous commit
Error was introduced with PR:

https://github.com/franko/lite-xl/pull/190

data/plugins/detectindent.lua
@@ -117,7 +117,7 @@ local function update_cache(doc)
   end
 end
 
-function Doc:on_text_change(...)
+function Doc.on_text_change(...)
   if current_on_text_change then
     current_on_text_change(...)
   end