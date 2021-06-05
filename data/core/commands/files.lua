a6f779d3ff8f30b884538fa647ffe2cf62add8c5
takase1121 <20792268+takase1121@users.noreply.github.com>
2021-06-05 09:25:25 +0800

    allow nested directories to be created

--- data/core/commands/files.lua
+++ data/core/commands/files.lua
@@ -1,13 +1,36 @@
 local core = require "core"
 local command = require "core.command"
 
+local function mkdirp(path)
+  local segments = {}
+  if path == '/' or path == '\\' then
+    table.insert(segments, PATHSEP)
+  end
+
+  for m in string.gmatch("[^/\\]+") do
+    table.insert(segments, m)
+  end
+
+  if string.match(path, "^[/\\]") then
+    segments[1] = PATHSEP .. segments[1]
+  end
+
+  for i = 1, #segments do
+    local p = table.concat(segments, PATHSEP, 1, i)
+    if not system.get_file_info(p) then
+      local success, err = system.mkdir(p)
+      if not success then
+        core.error("cannot create directory %q: %s", p, err)
+        break
+      end
+    end
+  end
+end
+
 command.add(nil, {
   ["files:create-directory"] = function()
     core.command_view:enter("New directory name", function(text)
-      local success, err = system.mkdir(text)
-      if not success then
-        core.error("cannot create directory %q: %s", text, err)
-      end
+      mkdirp(text)
     end)
   end,
 })
