3f58e554ba8730bf4a6e862e3096a4d13d212288
redtide <redtid3@gmail.com>
2021-06-20 17:37:08 +0200

    Reorganization of data resources

--- src/main.c
+++ src/main.c
@@ -79,7 +79,7 @@ static void get_exe_filename(char *buf, int sz) {
 
 static void init_window_icon(void) {
 #ifndef _WIN32
-  #include "../icon.inl"
+  #include "../resources/icons/icon.inl"
   (void) icon_rgba_len; /* unused */
   SDL_Surface *surf = SDL_CreateRGBSurfaceFrom(
     icon_rgba, 64, 64,
