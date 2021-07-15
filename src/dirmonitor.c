e01f6fefe653bcaa9250a10e99ce12efccf8d6b8
Francesco Abbate <francesco.bbt@gmail.com>
2021-07-15 09:56:49 +0200

    Treat watch dir errors and fix various things
    Verity if dmon_watch returns an error.
    
    Add a check if an added file for which we received a create event is
    ignored based on the user's config.
    
    Add some explanatory comments in the code.

--- src/dirmonitor.c
+++ src/dirmonitor.c
@@ -11,6 +11,8 @@
 static void send_sdl_event(dmon_watch_id watch_id, dmon_action action, const char *filepath) {
   SDL_Event ev;
   const int size = strlen(filepath) + 1;
+  /* The string allocated below should be deallocated as soon as the event is
+     treated in the SDL main loop. */
   char *new_filepath = malloc(size);
   if (!new_filepath) return;
   memcpy(new_filepath, filepath, size);
@@ -23,7 +25,6 @@ static void send_sdl_event(dmon_watch_id watch_id, dmon_action action, const cha
 #endif
   SDL_zero(ev);
   ev.type = SDL_USEREVENT;
-  fprintf(stderr, "DEBUG: send watch_id: %d action; %d\n", watch_id.id, action); fflush(stderr);
   ev.user.code = ((watch_id.id & 0xffff) << 16) | (action & 0xffff);
   ev.user.data1 = new_filepath;
   SDL_PushEvent(&ev);
@@ -31,7 +32,8 @@ static void send_sdl_event(dmon_watch_id watch_id, dmon_action action, const cha
 
 void dirmonitor_init() {
   dmon_init();
-  /* FIXME: not needed ? */
+  /* In theory we should register our user event but since we
+     have just one type of user event this is not really needed. */
   /* sdl_dmon_event_type = SDL_RegisterEvents(1); */
 }
 
@@ -39,9 +41,11 @@ void dirmonitor_deinit() {
   dmon_deinit();
 }
 
-void dirmonitor_push_event(dmon_watch_id watch_id, dmon_action action, const char *filepath,
-  const char *oldfilepath)
+void dirmonitor_watch_callback(dmon_watch_id watch_id, dmon_action action, const char *rootdir,
+  const char *filepath, const char *oldfilepath, void *user)
 {
+  (void) rootdir;
+  (void) user;
   switch (action) {
   case DMON_ACTION_MOVE:
     send_sdl_event(watch_id, DMON_ACTION_DELETE, oldfilepath);
