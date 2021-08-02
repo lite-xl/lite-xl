1725b3ab834f79020c520080ac5184c6d1dfb122
takase1121 <20792268+takase1121@users.noreply.github.com>
2021-08-02 09:54:55 +0800

    revert config.lineguide option

--- data/plugins/lineguide.lua
+++ data/plugins/lineguide.lua
@@ -6,18 +6,16 @@ local DocView = require "core.docview"
 local draw_overlay = DocView.draw_overlay
 
 function DocView:draw_overlay(...)
-  if config.lineguide then
-    local ns = self:get_font():get_width_subpixel("n") * config.line_limit
-    local ss = self:get_font():subpixel_scale()
-    local offset = ns / ss
-    local x = self:get_line_screen_position(1) + offset
-    local y = self.position.y
-    local w = math.ceil(SCALE * 1)
-    local h = self.size.y
+  local ns = self:get_font():get_width_subpixel("n") * config.line_limit
+  local ss = self:get_font():subpixel_scale()
+  local offset = ns / ss
+  local x = self:get_line_screen_position(1) + offset
+  local y = self.position.y
+  local w = math.ceil(SCALE * 1)
+  local h = self.size.y
 
-    local color = style.guide or style.selection
-    renderer.draw_rect(x, y, w, h, color)
-  end
+  local color = style.guide or style.selection
+  renderer.draw_rect(x, y, w, h, color)
 
   draw_overlay(self, ...)
 end
