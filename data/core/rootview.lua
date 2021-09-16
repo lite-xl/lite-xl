6a3f59c423660f691f297b38b524610b35ec5395
Guldoman <giulio.lettieri@gmail.com>
2021-09-16 23:26:11 +0200

    Move single tab drawing to its own function

--- data/core/rootview.lua
+++ data/core/rootview.lua
@@ -513,6 +513,53 @@ function Node:update()
   end
 end
 
+function Node:draw_tab(text, is_active, is_hovered, is_close_hovered, x, y, w, h, standalone)
+  local ds = style.divider_size
+  local dots_width = style.font:get_width("…")
+  local color = style.dim
+  local padding_y = style.padding.y
+  renderer.draw_rect(x + w, y + padding_y, ds, h - padding_y * 2, style.dim)
+  if standalone then
+    renderer.draw_rect(x-1, y-1, w+2, h+2, style.background2)
+  end
+  if is_active then
+    color = style.text
+    renderer.draw_rect(x, y, w, h, style.background)
+    renderer.draw_rect(x + w, y, ds, h, style.divider)
+    renderer.draw_rect(x - ds, y, ds, h, style.divider)
+  end
+  local cx, cw, cspace = close_button_location(x, w)
+  local show_close_button = ((is_active or is_hovered) and not standalone and config.tab_close_button)
+  if show_close_button then
+    local close_style = is_close_hovered and style.text or style.dim
+    common.draw_text(style.icon_font, close_style, "C", nil, cx, y, 0, h)
+  end
+  if is_hovered then
+    color = style.text
+  end
+  local padx = style.padding.x
+  -- Normally we should substract "cspace" from text_avail_width and from the
+  -- clipping width. It is the padding space we give to the left and right of the
+  -- close button. However, since we are using dots to terminate filenames, we
+  -- choose to ignore "cspace" accepting that the text can possibly "touch" the
+  -- close button.
+  local text_avail_width = cx - x - padx
+  core.push_clip_rect(x, y, cx - x, h)
+  x, w = x + padx, w - padx * 2
+  local align = "center"
+  if style.font:get_width(text) > text_avail_width then
+    align = "left"
+    for i = 1, #text do
+      local reduced_text = text:sub(1, #text - i)
+      if style.font:get_width(reduced_text) + dots_width <= text_avail_width then
+        text = reduced_text .. "…"
+        break
+      end
+    end
+  end
+  common.draw_text(style.font, color, text, align, x, y, w, h)
+  core.pop_clip_rect()
+end
 
 function Node:draw_tabs()
   local x, y, w, h, scroll_padding = self:get_scroll_button_rect(1)
@@ -537,47 +584,9 @@ function Node:draw_tabs()
   for i = self.tab_offset, self.tab_offset + tabs_number - 1 do
     local view = self.views[i]
     local x, y, w, h = self:get_tab_rect(i)
-    local text = view:get_name()
-    local color = style.dim
-    local padding_y = style.padding.y
-    renderer.draw_rect(x + w, y + padding_y, ds, h - padding_y * 2, style.dim)
-    if view == self.active_view then
-      color = style.text
-      renderer.draw_rect(x, y, w, h, style.background)
-      renderer.draw_rect(x + w, y, ds, h, style.divider)
-      renderer.draw_rect(x - ds, y, ds, h, style.divider)
-    end
-    local cx, cw, cspace = close_button_location(x, w)
-    local show_close_button = ((view == self.active_view or i == self.hovered_tab) and config.tab_close_button)
-    if show_close_button then
-      local close_style = self.hovered_close == i and style.text or style.dim
-      common.draw_text(style.icon_font, close_style, "C", nil, cx, y, 0, h)
-    end
-    if i == self.hovered_tab then
-      color = style.text
-    end
-    local padx = style.padding.x
-    -- Normally we should substract "cspace" from text_avail_width and from the
-    -- clipping width. It is the padding space we give to the left and right of the
-    -- close button. However, since we are using dots to terminate filenames, we
-    -- choose to ignore "cspace" accepting that the text can possibly "touch" the
-    -- close button.
-    local text_avail_width = cx - x - padx
-    core.push_clip_rect(x, y, cx - x, h)
-    x, w = x + padx, w - padx * 2
-    local align = "center"
-    if style.font:get_width(text) > text_avail_width then
-      align = "left"
-      for i = 1, #text do
-        local reduced_text = text:sub(1, #text - i)
-        if style.font:get_width(reduced_text) + dots_width <= text_avail_width then
-          text = reduced_text .. "…"
-          break
-        end
-      end
-    end
-    common.draw_text(style.font, color, text, align, x, y, w, h)
-    core.pop_clip_rect()
+    self:draw_tab(view:get_name(), view == self.active_view,
+                  i == self.hovered_tab, i == self.hovered_close,
+                  x, y, w, h)
   end
 
   core.pop_clip_rect()
