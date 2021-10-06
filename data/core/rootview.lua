4e313d9fc57a9b838761b3b088f6b087d56cf9cb
Adam Harrison <adamdharrison@gmail.com>
2021-10-05 20:00:06 -0400

    Propogated mouse clicks correctly.

--- data/core/rootview.lua
+++ data/core/rootview.lua
@@ -877,7 +877,7 @@ function RootView:on_mouse_pressed(button, x, y, clicks)
   elseif not self.dragged_node then -- avoid sending on_mouse_pressed events when dragging tabs
     core.set_active_view(node.active_view)
     if not self.on_view_mouse_pressed(button, x, y, clicks) then
-      node.active_view:on_mouse_pressed(button, x, y, clicks)
+      return node.active_view:on_mouse_pressed(button, x, y, clicks)
     end
   end
 end
