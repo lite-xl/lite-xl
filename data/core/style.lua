4183d3b2fdf8ed8d96e37a89b7f5afa89914996b
Jipok <braaga@inbox.ru>
2021-12-20 01:27:53 +0500

    Add predefined colors to style: good, bad

--- data/core/style.lua
+++ data/core/style.lua
@@ -46,6 +46,8 @@ style.nagbar_text = { common.color "#FFFFFF" }
 style.nagbar_dim = { common.color "rgba(0, 0, 0, 0.45)" }
 style.drag_overlay = { common.color "rgba(255,255,255,0.1)" }
 style.drag_overlay_tab = { common.color "#93DDFA" }
+style.good = { common.color "#72b886" }
+style.bad = { common.color "#FFA94D" }
 
 style.syntax = {}
 style.syntax["normal"] = { common.color "#e1e1e6" }
