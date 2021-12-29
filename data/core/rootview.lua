e70bdc98bf5b02f39aa1ff1288eb718a684f528f
Aleksander Litynski <ajlitynski@gmail.com>
2021-12-29 12:54:49 -0500

    tab gutter needs to mark single clicks as consumed - if it doesn't the clicks falls through to the tab's view

--- data/core/rootview.lua
+++ data/core/rootview.lua
@@ -942,9 +942,12 @@ function RootView:on_mouse_pressed(button, x, y, clicks)
   elseif not self.dragged_node then -- avoid sending on_mouse_pressed events when dragging tabs
     core.set_active_view(node.active_view)
     local in_gutter = node:in_tab_area_gutter(x, y)
-    if in_gutter and button == "left" and clicks == 2 then
-      -- if the user double clicks inside the tab gutter, make a new document
-      command.perform "core:new-doc"
+    if in_gutter and button == "left" then
+      if clicks == 2 then
+        -- if the user double clicks inside the tab gutter, make a new document
+        command.perform "core:new-doc"
+      end
+      -- both single and double clicks in the tab gutter are consumed by the gutter, we just don't act on single clicks
       return true
     end
     if not node:in_tab_area(x, y) then
