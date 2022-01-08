f2c9eb3a496ec65d24394b0a7c7aeab2dd62b831
takase1121 <20792268+takase1121@users.noreply.github.com>
2022-01-08 15:31:53 +0800

    revamp lhelper script
    add lockfile support
    seperate lhelper install from package install

--- scripts/lhelper.sh
+++ scripts/lhelper.sh
@@ -14,8 +14,45 @@ show_help() {
   echo
 }
 
+install() {
+  local lhelper_prefix=$1
+  if [[ -f ${lhelper_prefix}/bin/lhelper ]]; then
+    echo "lhelper is already installed."
+  else
+    git clone https://github.com/franko/lhelper.git
+
+    # FIXME: This should be set in ~/.bash_profile if not using CI
+    # export PATH="${HOME}/.local/bin:${PATH}"
+    mkdir -p "${lhelper_prefix}/bin"
+    pushd lhelper; bash install "${lhelper_prefix}"; popd
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      CC=clang CXX=clang++ lhelper create lite-xl -n
+    else
+      lhelper create lite-xl -n
+    fi
+  fi
+}
+
+setup() {
+  # Not using $(lhelper activate lite-xl) to support CI
+  source "$(lhelper env-source lite-xl)"
+
+  while read -r line; do
+    lhelper install $line
+  done < resources/lhelper-lockfile
+
+  # Help MSYS2 to find the SDL2 include and lib directories to avoid errors
+  # during build and linking when using lhelper.
+  if [[ "$OSTYPE" == "msys" ]]; then
+    CFLAGS=-I${LHELPER_ENV_PREFIX}/include/SDL2
+    LDFLAGS=-L${LHELPER_ENV_PREFIX}/lib
+  fi
+}
+
 main() {
   local lhelper_prefix="$HOME/.local"
+  local cmd=""
 
   for i in "$@"; do
     case $i in
@@ -27,6 +64,14 @@ main() {
         lhelper_prefix="$2"
         echo "LHelper prefix set to: \"${lhelper_prefix}\""
         shift
+        shift
+        ;;
+      --install)
+        cmd="install"
+        shift
+        ;;
+      --setup)
+        cmd="setup"
         shift
         ;;
       --debug)
@@ -41,34 +86,13 @@ main() {
 
   if [[ -n $1 ]]; then show_help; exit 1; fi
 
-  if [[ ! -f ${lhelper_prefix}/bin/lhelper ]]; then
-
-    git clone https://github.com/franko/lhelper.git
-
-    # FIXME: This should be set in ~/.bash_profile if not using CI
-    # export PATH="${HOME}/.local/bin:${PATH}"
-    mkdir -p "${lhelper_prefix}/bin"
-    pushd lhelper; bash install "${lhelper_prefix}"; popd
-
-    if [[ "$OSTYPE" == "darwin"* ]]; then
-      CC=clang CXX=clang++ lhelper create lite-xl -n
-    else
-      lhelper create lite-xl -n
-    fi
-  fi
-
-  # Not using $(lhelper activate lite-xl) to support CI
-  source "$(lhelper env-source lite-xl)"
-
-  lhelper install freetype2
-  lhelper install sdl2 2.0.14-wait-event-timeout-1
-  lhelper install pcre2
-
-  # Help MSYS2 to find the SDL2 include and lib directories to avoid errors
-  # during build and linking when using lhelper.
-  if [[ "$OSTYPE" == "msys" ]]; then
-    CFLAGS=-I${LHELPER_ENV_PREFIX}/include/SDL2
-    LDFLAGS=-L${LHELPER_ENV_PREFIX}/lib
+  if [[ $cmd == "install" ]]; then
+    install $lhelper_prefix
+  elif [[ $cmd == "setup" ]]; then
+    setup
+  else
+    show_help
+    exit 1
   fi
 }
 
