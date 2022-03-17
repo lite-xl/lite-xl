38e2d335948e48c97f688ccc34393f8d05079049
Jan200101 <sentrycraft123@gmail.com>
2022-03-17 09:56:59 +0100

    seperate dirmonitor logic, add build time detection of features
    this also adds libkqueue support

--- src/api/dirmonitor.c
+++ src/api/dirmonitor.c
@@ -15,144 +15,31 @@
 #include <string.h>
 #include <stdbool.h>
 
-/*
-This is *slightly* a clusterfuck. Normally, we'd
-have windows wait on a list of handles like inotify,
-however, MAXIMUM_WAIT_OBJECTS is 64. Yes, seriously.
-
-So, for windows, we are recursive.
-*/
-struct dirmonitor {
-  int fd;
-  #if _WIN32
-    HANDLE handle;
-    char buffer[8192];
-    OVERLAPPED overlapped;
-    bool running;
-  #endif
-};
-
-struct dirmonitor* init_dirmonitor() {
-  struct dirmonitor* monitor = calloc(sizeof(struct dirmonitor), 1);
-  #ifndef _WIN32
-    #if __linux__
-      monitor->fd = inotify_init1(IN_NONBLOCK);
-    #else
-      monitor->fd = kqueue();
-    #endif
-  #endif
-  return monitor;
-}
-
-#if _WIN32
-static void close_monitor_handle(struct dirmonitor* monitor) {
-  if (monitor->handle) {
-      if (monitor->running) {
-        BOOL result = CancelIoEx(monitor->handle, &monitor->overlapped);
-        DWORD error = GetLastError();
-        if (result == TRUE || error != ERROR_NOT_FOUND) {
-          DWORD bytes_transferred;
-          GetOverlappedResult( monitor->handle, &monitor->overlapped, &bytes_transferred, TRUE );
-        }
-        monitor->running = false;
-      }
-      CloseHandle(monitor->handle);
-    }
-  monitor->handle = NULL;
-}
+#ifndef DIRMONITOR_BACKEND
+#error No dirmonitor backend defined
 #endif
 
-void deinit_dirmonitor(struct dirmonitor* monitor) {
-  #if _WIN32
-    close_monitor_handle(monitor);
-  #else
-    close(monitor->fd);
-  #endif
-  free(monitor);
-}
+#define GLUE_HELPER(x, y) x##y
+#define GLUE(x, y) GLUE_HELPER(x, y)
 
-int check_dirmonitor(struct dirmonitor* monitor, int (*change_callback)(int, const char*, void*), void* data) {
-  #if _WIN32
-    if (!monitor->running) {
-      if (ReadDirectoryChangesW(monitor->handle, monitor->buffer, sizeof(monitor->buffer), TRUE,  FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME, NULL, &monitor->overlapped, NULL) == 0)
-        return GetLastError();
-      monitor->running = true;
-    }
-    DWORD bytes_transferred;
-    if (!GetOverlappedResult(monitor->handle, &monitor->overlapped, &bytes_transferred, FALSE)) {
-      int error = GetLastError();
-      return error == ERROR_IO_PENDING || error == ERROR_IO_INCOMPLETE ? 0 : error;
-    }
-    monitor->running = false;
-    for (FILE_NOTIFY_INFORMATION* info = (FILE_NOTIFY_INFORMATION*)monitor->buffer; (char*)info < monitor->buffer + sizeof(monitor->buffer); info = (FILE_NOTIFY_INFORMATION*)((char*)info) + info->NextEntryOffset) {
-      change_callback(info->FileNameLength, (char*)info->FileName, data);
-      if (!info->NextEntryOffset)
-        break;
-    }
-    monitor->running = false;
-    return 0;
-  #elif __linux__
-    char buf[PATH_MAX + sizeof(struct inotify_event)];
-    ssize_t offset = 0;
-    while (1) {
-      ssize_t len = read(monitor->fd, &buf[offset], sizeof(buf) - offset);
-      if (len == -1 && errno != EAGAIN)
-        return errno;
-      if (len <= 0)
-        return 0;
-      while (len > sizeof(struct inotify_event) && len >= ((struct inotify_event*)buf)->len + sizeof(struct inotify_event)) {
-        change_callback(((const struct inotify_event *)buf)->wd, NULL, data);
-        len -= sizeof(struct inotify_event) + ((struct inotify_event*)buf)->len;
-        memmove(buf, &buf[sizeof(struct inotify_event) + ((struct inotify_event*)buf)->len], len);
-        offset = len;
-      }
-    }
-  #else
-    struct kevent event;
-    while (1) {
-      struct timespec tm = {0};
-      int nev = kevent(monitor->fd, NULL, 0, &event, 1, &tm);
-      if (nev == -1)
-        return errno;
-      if (nev <= 0)
-        return 0;
-      change_callback(event.ident, NULL, data);
-    }
-  #endif
-}
+#define init_dirmonitor GLUE(init_dirmonitor_, DIRMONITOR_BACKEND)
+#define deinit_dirmonitor GLUE(deinit_dirmonitor_, DIRMONITOR_BACKEND)
+#define check_dirmonitor GLUE(check_dirmonitor_, DIRMONITOR_BACKEND)
+#define add_dirmonitor GLUE(add_dirmonitor_, DIRMONITOR_BACKEND)
+#define remove_dirmonitor GLUE(remove_dirmonitor_, DIRMONITOR_BACKEND)
 
-int add_dirmonitor(struct dirmonitor* monitor, const char* path) {
-  #if _WIN32
-    close_monitor_handle(monitor);
-    monitor->handle = CreateFileA(path, FILE_LIST_DIRECTORY, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
-    if (monitor->handle && monitor->handle != INVALID_HANDLE_VALUE)
-      return 1;
-    monitor->handle = NULL;
-    return -1;
-  #elif __linux__
-    return inotify_add_watch(monitor->fd, path, IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
-  #else
-    int fd = open(path, O_RDONLY);
-    struct kevent change;
-    EV_SET(&change, fd, EVFILT_VNODE, EV_ADD | EV_CLEAR, NOTE_DELETE | NOTE_EXTEND | NOTE_WRITE | NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME, 0, (void*)path);
-    kevent(monitor->fd, &change, 1, NULL, 0, NULL);
-    return fd;
-  #endif
-}
+struct dirmonitor {}; // dirmonitor struct is defined in each backend
 
-void remove_dirmonitor(struct dirmonitor* monitor, int fd) {
-  #if _WIN32
-    close_monitor_handle(monitor);
-  #elif __linux__
-    inotify_rm_watch(monitor->fd, fd);
-  #else
-    close(fd);
-  #endif
-}
+// define functions so we know their signature
+struct dirmonitor* init_dirmonitor();
+void deinit_dirmonitor(struct dirmonitor*);
+int check_dirmonitor(struct dirmonitor*, int (*)(int, const char*, void*), void*);
+int add_dirmonitor(struct dirmonitor*, const char*);
+void remove_dirmonitor(struct dirmonitor*, int);
 
 static int f_check_dir_callback(int watch_id, const char* path, void* L) {
   lua_pushvalue(L, -1);
-  #if _WIN32
+  #ifdef DIRMONITOR_WIN32
     char buffer[PATH_MAX*4];
     int count = WideCharToMultiByte(CP_UTF8, 0, (WCHAR*)path, watch_id, buffer, PATH_MAX*4 - 1, NULL, NULL);
     lua_pushlstring(L, buffer, count);
