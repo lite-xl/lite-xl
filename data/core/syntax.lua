951f0913da8ce19a650218fe0a96c88784903a7c
Jefferson González <jgmdev@gmail.com>
2022-03-25 11:25:32 -0400

    syntax: add pattern to boost tokenizer performance (#896)

--- data/core/syntax.lua
+++ data/core/syntax.lua
@@ -7,6 +7,11 @@ local plain_text_syntax = { name = "Plain Text", patterns = {}, symbols = {} }
 
 
 function syntax.add(t)
+  -- this rule gives us a performance gain for the tokenizer in lines with
+  -- long amounts of consecutive spaces without affecting other patterns
+  if t.patterns then
+    table.insert(t.patterns, 1, { pattern = "%s+", type = "normal" })
+  end
   table.insert(syntax.items, t)
 end
 
