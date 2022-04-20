ea495f7c623d8f8cd0df372e1cb7d3861e2ee938
Adam Harrison <adamdharrison@gmail.com>
2022-04-19 20:51:44 -0400

    Made sure to properly cancel operations.

--- src/api/dirmonitor/win32.c
+++ src/api/dirmonitor/win32.c
@@ -7,10 +7,13 @@ struct dirmonitor_internal {
 
 
 int get_changes_dirmonitor(struct dirmonitor_internal* monitor, char* buffer, int buffer_size) {
-  DWORD bytes_transferred;
-  if (ReadDirectoryChangesW(monitor->handle, buffer, buffer_size, TRUE,  FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME, &bytes_transferred, NULL, NULL) == 0)
-    return -1;
-  return bytes_transferred;
+  if (monitor->handle && monitor->handle != INVALID_HANDLE_VALUE) {
+    DWORD bytes_transferred;
+    if (ReadDirectoryChangesW(monitor->handle, buffer, buffer_size, TRUE,  FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME, &bytes_transferred, NULL, NULL) == 0)
+      return 0;
+    return bytes_transferred;
+  }
+  return 0;
 }
 
 
@@ -20,7 +23,12 @@ struct dirmonitor* init_dirmonitor() {
 
 
 static void close_monitor_handle(struct dirmonitor_internal* monitor) {
-  CloseHandle(monitor->handle);
+  if (monitor->handle && monitor->handle != INVALID_HANDLE_VALUE) {
+    HANDLE handle = monitor->handle;
+    monitor->handle = NULL;
+    CancelIoEx(handle, NULL);
+    CloseHandle(handle);
+  }
 }
 
 
@@ -33,7 +41,7 @@ int translate_changes_dirmonitor(struct dirmonitor_internal* monitor, char* buff
   for (FILE_NOTIFY_INFORMATION* info = (FILE_NOTIFY_INFORMATION*)buffer; (char*)info < buffer + buffer_size; info = (FILE_NOTIFY_INFORMATION*)(((char*)info) + info->NextEntryOffset)) {
     char transform_buffer[PATH_MAX*4];
     int count = WideCharToMultiByte(CP_UTF8, 0, (WCHAR*)info->FileName, info->FileNameLength, transform_buffer, PATH_MAX*4 - 1, NULL, NULL);
-    change_callback(info->FileNameLength / sizeof(WCHAR), buffer, data);
+    change_callback(count, buffer, data);
     if (!info->NextEntryOffset)
       break;
   }
@@ -43,7 +51,7 @@ int translate_changes_dirmonitor(struct dirmonitor_internal* monitor, char* buff
 
 int add_dirmonitor(struct dirmonitor_internal* monitor, const char* path) {
   close_monitor_handle(monitor);
-  monitor->handle = CreateFileA(path, FILE_LIST_DIRECTORY, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
+  monitor->handle = CreateFileA(path, FILE_LIST_DIRECTORY, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
   return !monitor->handle || monitor->handle == INVALID_HANDLE_VALUE ? -1 : 1;
 }
 
