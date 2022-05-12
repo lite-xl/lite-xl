9694169c680420d73799ae23e5d70fca545fab63
rcz <car520120@163.com>
2022-05-13 01:26:31 +0800

    sync merge utf8extra

--- data/core/utf8string.lua
+++ data/core/utf8string.lua
@@ -13,7 +13,6 @@ string.ureverse = utf8extra.reverse
 string.usub = utf8extra.sub
 string.uupper = utf8extra.upper
 
-<<<<<<< HEAD
 string.uescape = utf8extra.escape
 string.ucharpos = utf8extra.charpos
 string.unext = utf8extra.next
@@ -24,21 +23,6 @@ string.uwidthindex = utf8extra.widthindex
 string.utitle = utf8extra.title
 string.ufold = utf8extra.fold
 string.uncasecmp = utf8extra.ncasecmp
-=======
-local utf8 = require "utf8extra"
-
-string.ubyte = utf8.byte
-string.uchar = utf8.char
-string.ufind = utf8.find
-string.ugmatch = utf8.gmatch
-string.ugsub = utf8.gsub
-string.ulen = utf8.len
-string.ulower = utf8.lower
-string.umatch = utf8.match
-string.ureverse = utf8.reverse
-string.usub = utf8.sub
-string.uupper = utf8.upper
->>>>>>> lite-xl/master
 
 string.uoffset = utf8extra.offset
 string.ucodepoint = utf8extra.codepoint
