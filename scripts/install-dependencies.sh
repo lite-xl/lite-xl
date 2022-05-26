90870c62619b1d1cd2474b564de8dd37b4803419
jgmdev <jgmdev@gmail.com>
2022-05-26 02:04:30 -0400

    install wayland-protocols

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -48,7 +48,7 @@ main() {
     if [[ $lhelper == true ]]; then
       sudo apt-get install -qq ninja-build
     else
-      sudo apt-get install -qq ninja-build libsdl2-dev libfreetype6
+      sudo apt-get install -qq ninja-build wayland-protocols libsdl2-dev libfreetype6
     fi
     pip3 install meson
   elif [[ "$OSTYPE" == "darwin"* ]]; then
