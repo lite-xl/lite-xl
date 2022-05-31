2d8a15f3ab6ed9d471c89e5c69fc3ed438903b0e
jgmdev <jgmdev@gmail.com>
2022-05-31 16:34:14 -0400

    plugins: dropped --lite-xl version tag

--- data/plugins/detectindent.lua
+++ data/plugins/detectindent.lua
@@ -1,4 +1,4 @@
--- mod-version:3 -- lite-xl 2.1
+-- mod-version:3
 local core = require "core"
 local command = require "core.command"
 local common = require "core.common"
--- data/plugins/language_md.lua
+++ data/plugins/language_md.lua
@@ -1,4 +1,4 @@
--- mod-version:3 -- lite-xl 2.1
+-- mod-version:3
 local syntax = require "core.syntax"
 local style = require "core.style"
 local core = require "core"
