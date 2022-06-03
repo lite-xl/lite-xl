2535c83cd39cd4ecb68df8b1e677a49cb9a997f6
jgmdev <jgmdev@gmail.com>
2022-06-03 14:16:21 -0400

    language lua: some improvements

--- data/plugins/language_lua.lua
+++ data/plugins/language_lua.lua
@@ -27,9 +27,53 @@ syntax.add {
     { pattern = "%.%.%.?",                   type = "operator" },
     { pattern = "[<>~=]=",                   type = "operator" },
     { pattern = "[%+%-=/%*%^%%#<>]",         type = "operator" },
-    { pattern = "[%a_][%w_]*()%s*%f[(\"'{]", type = {"function", "normal"} },
-    { pattern = "[%a_][%w_]*",               type = "symbol" },
+    -- Metamethods
+    { pattern = "__[%a][%w_]+",              type = "function" },
+    { pattern = "%.()__[%a][%w_]+",
+      type = { "normal", "function" }
+    },
+    -- Variable declaration
+    { pattern = "local()%s+()[%a_][%w_]+",
+      type = { "operator", "normal", "keyword2" }
+    },
+    -- Multiple variable declarations
+    { pattern = "[%a][%w_]*()%s*,%s*()[%a][%w_]*() *=",
+      type = { "keyword2", "normal", "keyword2", "operator" }
+    },
+    { pattern = "[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*=",
+      type = { "keyword2", "normal", "keyword2", "normal", "keyword2", "operator" }
+    },
+    { pattern = "[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*=",
+      type = { "keyword2", "normal", "keyword2", "normal", "keyword2", "normal", "keyword2", "operator" }
+    },
+    { pattern = "[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*,%s*()[%a][%w_]*()%s*,%s*()[%a][%w_]*%s*=",
+      type = { "keyword2", "normal", "keyword2", "normal", "keyword2", "normal", "keyword2", "normal", "keyword2", "operator", }
+    },
+    -- Function calls
+    { pattern = "[%a_][%w_]+()%s*%f[(\"'{]", type = {"function", "normal"} },
+    { pattern = "[%a_][%w_%.]+()%.()[%a_][%w_]+()%s*%f[(\"'{]",
+      type = { "normal", "normal", "function", "normal"} },
+    -- Sub fields
+    { pattern = "%.()[%a_][%w_]*%f[^%.%w=%(]",
+      type = { "normal", "literal" }
+    },
+    -- Uppercase constants of at least 2 chars in len
+    {
+        pattern = "%u[%u_][%u%d_]*%f[%s%+%*%-%.%(%)%?%^%%=/<>~|&:,~]",
+        type = "number"
+    },
+    -- Variable assignments
+    { pattern = "%a[%w_]*() *= *%f[^=]",
+      type = { "keyword2", "operator" }
+    },
+    -- Placeholder
+    { pattern = "_(),",
+      type = { "operator", "normal" }
+    },
+    -- Labels
     { pattern = "::[%a_][%w_]*::",           type = "function" },
+    -- Other text
+    { pattern = "[%a_][%w_]*",               type = "normal" },
   },
   symbols = {
     ["if"]       = "keyword",
@@ -52,9 +96,9 @@ syntax.add {
     ["or"]       = "keyword",
     ["goto"]     = "keyword",
     ["self"]     = "keyword2",
-    ["true"]     = "literal",
-    ["false"]    = "literal",
-    ["nil"]      = "literal",
+    ["true"]     = "number",
+    ["false"]    = "number",
+    ["nil"]      = "number",
   },
 }
 
