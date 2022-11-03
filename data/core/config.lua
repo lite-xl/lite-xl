4dc27ce5dc9bc199ee5f4d09ce0032f78f4cbfe9
Guldoman <giulio.lettieri@gmail.com>
2022-11-03 17:40:27 +0100

    Add `config.keep_newline_whitespace` option (#1184)
    This option will avoid removing line content when pressing enter in 
    lines with only whitespace.

--- data/core/config.lua
+++ data/core/config.lua
@@ -30,6 +30,7 @@ config.highlight_current_line = true
 config.line_height = 1.2
 config.indent_size = 2
 config.tab_type = "soft"
+config.keep_newline_whitespace = false
 config.line_limit = 80
 config.max_project_files = 2000
 config.transitions = true
