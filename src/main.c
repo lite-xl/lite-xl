5c2ae2363ba24954bb7de49457ed36c0917e7038
pkubaj <pkubaj@FreeBSD.org>
2022-11-26 18:13:19 +0000

    Fix build on ppc(64)*

--- src/main.c
+++ src/main.c
@@ -99,6 +99,10 @@ void set_macos_bundle_resources(lua_State *L);
     #define ARCH_PROCESSOR "aarch64"
   #elif defined(__arm__) || defined(_M_ARM)
     #define ARCH_PROCESSOR "arm"
+  #elif defined(__powerpc64__)
+    #define ARCH_PROCESSOR "ppc64"
+  #elif defined(__powerpc__)
+    #define ARCH_PROCESSOR "ppc"
   #endif
 
   #if _WIN32
