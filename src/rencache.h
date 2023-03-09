1d0725f9044eb556170ca2d14ca2f14d88fa4efb
Guldoman <giulio.lettieri@gmail.com>
2023-03-09 16:48:46 +0100

    Improve text width calculation precision (#1408)
    In some extreme cases (~30000 chars) text width precision takes a hit.
    Using double instead of float fixes that.

--- src/rencache.h
+++ src/rencache.h
@@ -8,7 +8,7 @@
 void  rencache_show_debug(bool enable);
 void  rencache_set_clip_rect(RenRect rect);
 void  rencache_draw_rect(RenRect rect, RenColor color);
-float rencache_draw_text(RenWindow *window_renderer, RenFont **font, const char *text, size_t len, float x, int y, RenColor color);
+double rencache_draw_text(RenWindow *window_renderer, RenFont **font, const char *text, size_t len, double x, int y, RenColor color);
 void  rencache_invalidate(void);
 void  rencache_begin_frame(RenWindow *window_renderer);
 void  rencache_end_frame(RenWindow *window_renderer);
