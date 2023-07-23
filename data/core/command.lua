803229574ad0da83fee225c73085a800cfd56b8f
takase1121 <20792268+takase1121@users.noreply.github.com>
2023-07-23 12:26:09 +0800

    docs(core.command): add documentation

--- data/core/command.lua
+++ data/core/command.lua
@@ -1,25 +1,46 @@
 local core = require "core"
 local command = {}
 
+---A predicate function accepts arguments from `command.perform()` and evaluates to a boolean. </br>
+---If the function returns true, then the function associated with the command is executed.
+---
+---The predicate function can also return other values after the boolean, which will
+---be passed into the function associated with the command.
+---@alias command.predicate_function fun(...: any): boolean, ...
+
+---A predicate is a string, Object or a function that is used to determine whether a command should be
+---executed. It could be used to match the current view among many other things.
+---
+---If the predicate is a string, it is resolved into an `Object` via `require()`
+---and checked against the active view with `Object:extends()`. </br>
+---For example, `"core.docview"` will match any view that inherits from `DocView`. </br>
+---A `!` can be appended to the predicate to strictly match the current view via `Object:is()`,
+---instead of matching any view that inherits the predicate.
+---
+---If the predicate is a table, it it checked against the active view with `Object:extends()`.
+---Strict matching via `Object:is()` is not available.
+---
+---If the predicate is a function, it must be a predicate function - a function that takes in
+---any arguments that may be passed via `command.perform()`, and returns a boolean and any
+---other values for the function associated to the command.
+---@alias command.predicate string|core.object|command.predicate_function
+
+---The predicate and its associated function.
+---@class command.command
+---@field predicate command.predicate_function
+---@field perform fun(...: any)
+
+---@type { [string]: command.command }
 command.map = {}
 
 local always_true = function() return true end
 
 
----Used iternally by command.add, statusview, and contextmenu to generate a
----function with a condition to evaluate returning the boolean result of this
----evaluation.
----
----If a string predicate is given it is treated as a require import that should
----return a valid object which is checked against the current active view,
----eg: "core.docview" will match any view that inherits from DocView. Appending
----a `!` at the end of the string means we want to match the given object
----from the import strcitly eg: "core.docview!" only DocView is matched.
----A function that returns a boolean can be used instead to perform a custom
----evaluation, setting to nil means always evaluates to true.
----
----@param predicate string | table | function
----@return function
+---This function takes in a predicate and produces a predicate function
+---that is internally used to dispatch and execute commands.
+---@see command.predicate
+---@param predicate command.predicate
+---@return command.predicate_function
 function command.generate_predicate(predicate)
   predicate = predicate or always_true
   local strict = false
@@ -42,6 +63,14 @@ function command.generate_predicate(predicate)
 end
 
 
+---Adds commands to the map.
+---
+---The function accepts a table containing a list of commands
+---and their functions. </br>
+---If a command already exists, it will be replaced.
+---@see command.predicate
+---@param predicate command.predicate
+---@param map { [string]: fun(...: any) }
 function command.add(predicate, map)
   predicate = command.generate_predicate(predicate)
   for name, fn in pairs(map) do
@@ -57,11 +86,19 @@ local function capitalize_first(str)
   return str:sub(1, 1):upper() .. str:sub(2)
 end
 
+---Prettifies the command name.
+---
+---This function adds a space between the colon and the command name,
+---replaces dashes withe spaces and capitalizes the command appropriately.
+---@param name string
+---@return string
 function command.prettify_name(name)
   return name:gsub(":", ": "):gsub("-", " "):gsub("%S+", capitalize_first)
 end
 
 
+---Returns all commands that can be executed (their predicates evaluate to true).
+---@return string[]
 function command.get_all_valid()
   local res = {}
   local memoized_predicates = {}
@@ -76,6 +113,10 @@ function command.get_all_valid()
   return res
 end
 
+---Checks whether a command can be executed (it's predicate evaluates to true).
+---@param name string
+---@param ... any
+---@return boolean
 function command.is_valid(name, ...)
   return command.map[name] and command.map[name].predicate(...)
 end
@@ -98,12 +139,25 @@ local function perform(name, ...)
 end
 
 
+---Performs a command.
+---
+---The arguments passed into this function is forwarded to the predicate function. </br>
+---If the predicate function returns more than 1 value, the second to last values
+---are passed to the function associated to the command.
+---
+---Otherwise, the the arguments passed into this function is passed directly
+---to the function associated with the command.
+---@see command.predicate
+---@see command.predicate_function
+---@param ... any
+---@return boolean # true if the command is performed successfully.
 function command.perform(...)
   local ok, res = core.try(perform, ...)
   return not ok or res
 end
 
 
+---Inserts the default commands for Lite XL into the map.
 function command.add_defaults()
   local reg = {
     "core", "root", "command", "doc", "findreplace",
