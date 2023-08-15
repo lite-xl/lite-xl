6bb3a60744846970ed9e40e10af84c137fedbba4
takase1121 <20792268+takase1121@users.noreply.github.com>
2023-08-15 11:00:08 +0800

    feat(bootstrap): implement clib, croot and lite-xl searcher in Lua

--- data/core/start.lua
+++ data/core/start.lua
@@ -37,11 +37,47 @@ package.cpath =
   DATADIR .. '/?/init.' .. suffix .. ";"
 
 package.native_plugins = {}
-package.searchers = { package.searchers[1], package.searchers[2], function(modname)
+
+local function sanitize_entrypoint(name)
+  return name:match("[^%-]*"):gsub("%.", "_")
+end
+
+local function load_function(modname, path)
+  local err_msg = {}
+  local entrypoint = sanitize_entrypoint(modname)
+  for prefix in ipairs { "luaopen_lite_xl_", "luaopen_" } do
+    local fn, err = system.loadlib(path, prefix .. entrypoint, prefix == "luaopen_lite_xl_")
+    if not fn then
+      err_msg[#err_msg+1] = string.format("%s:%s%s: %s", path, prefix, entrypoint, err)
+    else
+      return fn
+    end
+  end
+  return "no symbol '" .. table.concat(err_msg, "'\n\tno symbol '")
+end
+
+---The normal C library searcher, implemented with system.loadlib.
+local function clib_searcher(modname)
   local path, err = package.searchpath(modname, package.cpath)
-  if not path then return err end
-  return system.load_native_plugin, path
-end }
+  return (path and load_function(modname, path) or err), path
+end
+
+---The normal C root (all-in-one) searcher, implemented with system.loadlib.
+local function croot_searcher(modname)
+  local root = modname:match("^[^%.]*")
+  local path, err = package.searchpath(root, package.cpath)
+  return (path and load_function(modname, path) or err), path
+end
+
+---Lite XL's own C library searcher.
+---This searcher searches for the library file normally, but only uses the last part of the module to resolve symbols.
+local function lite_xl_searcher(modname)
+  local mod = modname:match("[^%.]*$")
+  local path, err = package.searchpath(modname, package.cpath)
+  return (path and load_function(mod, path) or err), path
+end
+
+package.searchers = { package.searchers[1], package.searchers[2], clib_searcher, croot_searcher, lite_xl_searcher }
 
 table.pack = table.pack or pack or function(...) return {...} end
 table.unpack = table.unpack or unpack
