43c5d40e9625f111f20f3daf23c11687b63a1edd
takase1121 <20792268+takase1121@users.noreply.github.com>
2023-11-10 08:47:08 +0800

    refactor(appdmg.sh): don't generate config on the fly

--- scripts/appdmg.sh
+++ scripts/appdmg.sh
@@ -6,22 +6,4 @@ if [ ! -e "src/api/api.h" ]; then
   exit 1
 fi
 
-cat > lite-xl-dmg.py << EOF
-import os.path
-
-app_path = "$(pwd)/Lite XL.app"
-app_name = os.path.basename(app_path)
-
-format = defines.get("format", "UDBZ")
-
-files = [ (app_path, app_name) ]
-symlinks = { "Applications": "/Applications" }
-icon_locations = { app_name: (144, 248), "Applications": (336, 248) }
-
-icon = "$(pwd)/resources/icons/icon.icns"
-background = "$(pwd)/resources/macos/appdmg.png"
-window_rect = ((360, 360), (480, 360))
-default_view = "icon_view"
-EOF
-
-dmgbuild -s lite-xl-dmg.py "Lite XL" "$(pwd)/$1.dmg"
+dmgbuild -s resources/macos/lite-xl-dmg.py "Lite XL" "$1.dmg"
