028294af4095d6a0ec3cb5f658ef12886c893de6
Adam Harrison <adamdharrison@gmail.com>
2023-12-02 13:31:21 -0500

    Restored logic to create bundle.

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -6,4 +6,13 @@ if [ ! -e "src/api/api.h" ]; then
   exit 1
 fi
 
-dmgbuild -s resources/macos/lite-xl-dmg.py "Lite XL" "$1.dmg"
+mkdir -p "Lite XL.app/Contents/MacOS" "Lite XL.app/Contents/Resources"
+cp -r $1/lite-xl/lite-xl "Lite XL.app/Contents/MacOS"
+cp -r $1/lite-xl/data/* "Lite XL.app/Contents/Resources"
+cp resources/icons/icon.icns "Lite XL.app/Contents/Resources"
+cp -r $1/Info.plist "Lite XL.app/Contents/Resources"
+
+dmgbuild -s resources/macos/lite-xl-dmg.py "Lite XL" "$2.dmg"
+
+rm -rf "Lite XL.app"
+
