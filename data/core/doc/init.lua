b346e47738495947ca0aa06282f66e27f0110b71
Adam Harrison <adamdharrison@gmail.com>
2023-12-06 08:58:10 -0500

    Virtual lines begins.

--- data/core/doc/init.lua
+++ data/core/doc/init.lua
@@ -21,6 +21,7 @@ end
 
 function Doc:new(filename, abs_filename, new_file)
   self.new_file = new_file
+  self.listeners = {}
   self:reset()
   if filename then
     self:set_filename(filename, abs_filename)
@@ -142,138 +143,6 @@ function Doc:get_change_id()
   return self.undo_stack.idx
 end
 
-local function sort_positions(line1, col1, line2, col2)
-  if line1 > line2 or line1 == line2 and col1 > col2 then
-    return line2, col2, line1, col1, true
-  end
-  return line1, col1, line2, col2, false
-end
-
--- Cursor section. Cursor indices are *only* valid during a get_selections() call.
--- Cursors will always be iterated in order from top to bottom. Through normal operation
--- curors can never swap positions; only merge or split, or change their position in cursor
--- order.
-function Doc:get_selection(sort)
-  local line1, col1, line2, col2, swap = self:get_selection_idx(self.last_selection, sort)
-  if not line1 then
-    line1, col1, line2, col2, swap = self:get_selection_idx(1, sort)
-  end
-  return line1, col1, line2, col2, swap
-end
-
----Get the selection specified by `idx`
----@param idx integer @the index of the selection to retrieve
----@param sort? boolean @whether to sort the selection returned
----@return integer,integer,integer,integer,boolean? @line1, col1, line2, col2, was the selection sorted
-function Doc:get_selection_idx(idx, sort)
-  local line1, col1, line2, col2 = self.selections[idx * 4 - 3], self.selections[idx * 4 - 2],
-      self.selections[idx * 4 - 1],
-      self.selections[idx * 4]
-  if line1 and sort then
-    return sort_positions(line1, col1, line2, col2)
-  else
-    return line1, col1, line2, col2
-  end
-end
-
-function Doc:get_selection_text(limit)
-  limit = limit or math.huge
-  local result = {}
-  for idx, line1, col1, line2, col2 in self:get_selections() do
-    if idx > limit then break end
-    if line1 ~= line2 or col1 ~= col2 then
-      local text = self:get_text(line1, col1, line2, col2)
-      if text ~= "" then result[#result + 1] = text end
-    end
-  end
-  return table.concat(result, "\n")
-end
-
-function Doc:has_selection()
-  local line1, col1, line2, col2 = self:get_selection(false)
-  return line1 ~= line2 or col1 ~= col2
-end
-
-function Doc:has_any_selection()
-  for idx, line1, col1, line2, col2 in self:get_selections() do
-    if line1 ~= line2 or col1 ~= col2 then return true end
-  end
-  return false
-end
-
-function Doc:sanitize_selection()
-  for idx, line1, col1, line2, col2 in self:get_selections() do
-    self:set_selections(idx, line1, col1, line2, col2)
-  end
-end
-
-function Doc:set_selections(idx, line1, col1, line2, col2, swap, rm)
-  assert(not line2 == not col2, "expected 3 or 5 arguments")
-  if swap then line1, col1, line2, col2 = line2, col2, line1, col1 end
-  line1, col1 = self:sanitize_position(line1, col1)
-  line2, col2 = self:sanitize_position(line2 or line1, col2 or col1)
-  common.splice(self.selections, (idx - 1) * 4 + 1, rm == nil and 4 or rm, { line1, col1, line2, col2 })
-end
-
-function Doc:add_selection(line1, col1, line2, col2, swap)
-  local l1, c1 = sort_positions(line1, col1, line2 or line1, col2 or col1)
-  local target = #self.selections / 4 + 1
-  for idx, tl1, tc1 in self:get_selections(true) do
-    if l1 < tl1 or l1 == tl1 and c1 < tc1 then
-      target = idx
-      break
-    end
-  end
-  self:set_selections(target, line1, col1, line2, col2, swap, 0)
-  self.last_selection = target
-end
-
-function Doc:remove_selection(idx)
-  if self.last_selection >= idx then
-    self.last_selection = self.last_selection - 1
-  end
-  common.splice(self.selections, (idx - 1) * 4 + 1, 4)
-end
-
-function Doc:set_selection(line1, col1, line2, col2, swap)
-  self.selections = {}
-  self:set_selections(1, line1, col1, line2, col2, swap)
-  self.last_selection = 1
-end
-
-function Doc:merge_cursors(idx)
-  for i = (idx or (#self.selections - 3)), (idx or 5), -4 do
-    for j = 1, i - 4, 4 do
-      if self.selections[i] == self.selections[j] and
-          self.selections[i + 1] == self.selections[j + 1] then
-        common.splice(self.selections, i, 4)
-        if self.last_selection >= (i + 3) / 4 then
-          self.last_selection = self.last_selection - 1
-        end
-        break
-      end
-    end
-  end
-end
-
-local function selection_iterator(invariant, idx)
-  local target = invariant[3] and (idx * 4 - 7) or (idx * 4 + 1)
-  if target > #invariant[1] or target <= 0 or (type(invariant[3]) == "number" and invariant[3] ~= idx - 1) then return end
-  if invariant[2] then
-    return idx + (invariant[3] and -1 or 1), sort_positions(table.unpack(invariant[1], target, target + 4))
-  else
-    return idx + (invariant[3] and -1 or 1), table.unpack(invariant[1], target, target + 4)
-  end
-end
-
--- If idx_reverse is true, it'll reverse iterate. If nil, or false, regular iterate.
--- If a number, runs for exactly that iteration.
-function Doc:get_selections(sort_intra, idx_reverse)
-  return selection_iterator, { self.selections, sort_intra, idx_reverse },
-      idx_reverse == true and ((#self.selections / 4) + 1) or ((idx_reverse or -1) + 1)
-end
-
--- End of cursor seciton.
 
 function Doc:sanitize_position(line, col)
   local nlines = #self.lines
@@ -365,7 +234,6 @@ local function pop_undo(self, undo_stack, redo_stack, modified)
     self:raw_remove(line1, col1, line2, col2, redo_stack, cmd.time)
   elseif cmd.type == "selection" then
     self.selections = { table.unpack(cmd) }
-    self:sanitize_selection()
   end
 
   modified = modified or (cmd.type ~= "selection")
@@ -398,29 +266,19 @@ function Doc:raw_insert(line, col, text, undo_stack, time)
   -- splice lines into line array
   common.splice(self.lines, line, 1, lines)
 
-  -- keep cursors where they should be
-  for idx, cline1, ccol1, cline2, ccol2 in self:get_selections(true, true) do
-    if cline1 < line then break end
-    local line_addition = (line < cline1 or col < ccol1) and #lines - 1 or 0
-    local column_addition = line == cline1 and ccol1 > col and len or 0
-    self:set_selections(idx, cline1 + line_addition, ccol1 + column_addition, cline2 + line_addition,
-      ccol2 + column_addition)
-  end
 
   -- push undo
   local line2, col2 = self:position_offset(line, col, #text)
-  push_undo(undo_stack, time, "selection", table.unpack(self.selections))
   push_undo(undo_stack, time, "remove", line, col, line2, col2)
 
   -- update highlighter and assure selection is in bounds
   self.highlighter:insert_notify(line, #lines - 1)
-  self:sanitize_selection()
+  for i,v in ipairs(listeners) do v(text, line, col, line, col) end
 end
 
 function Doc:raw_remove(line1, col1, line2, col2, undo_stack, time)
   -- push undo
   local text = self:get_text(line1, col1, line2, col2)
-  push_undo(undo_stack, time, "selection", table.unpack(self.selections))
   push_undo(undo_stack, time, "insert", line1, col1, text)
 
   -- get line content before/after removed text
@@ -435,44 +293,8 @@ function Doc:raw_remove(line1, col1, line2, col2, undo_stack, time)
 
   local merge = false
 
-  -- keep selections in correct positions: each pair (line, col)
-  -- * remains unchanged if before the deleted text
-  -- * is set to (line1, col1) if in the deleted text
-  -- * is set to (line1, col - col_removal) if on line2 but out of the deleted text
-  -- * is set to (line - line_removal, col) if after line2
-  for idx, cline1, ccol1, cline2, ccol2 in self:get_selections(true, true) do
-    if cline2 < line1 then break end
-    local l1, c1, l2, c2 = cline1, ccol1, cline2, ccol2
-
-    if cline1 > line1 or (cline1 == line1 and ccol1 > col1) then
-      if cline1 > line2 then
-        l1 = l1 - line_removal
-      else
-        l1 = line1
-        c1 = (cline1 == line2 and ccol1 > col2) and c1 - col_removal or col1
-      end
-    end
-
-    if cline2 > line1 or (cline2 == line1 and ccol2 > col1) then
-      if cline2 > line2 then
-        l2 = l2 - line_removal
-      else
-        l2 = line1
-        c2 = (cline2 == line2 and ccol2 > col2) and c2 - col_removal or col1
-      end
-    end
-
-    if l1 == line1 and c1 == col1 then merge = true end
-    self:set_selections(idx, l1, c1, l2, c2)
-  end
-
-  if merge then
-    self:merge_cursors()
-  end
-
   -- update highlighter and assure selection is in bounds
   self.highlighter:remove_notify(line1, line_removal)
-  self:sanitize_selection()
 end
 
 function Doc:insert(line, col, text)
@@ -503,99 +325,6 @@ function Doc:redo()
   pop_undo(self, self.redo_stack, self.undo_stack, false)
 end
 
-function Doc:text_input(text, idx)
-  for sidx, line1, col1, line2, col2 in self:get_selections(true, idx or true) do
-    local had_selection = false
-    if line1 ~= line2 or col1 ~= col2 then
-      self:delete_to_cursor(sidx)
-      had_selection = true
-    end
-
-    if self.overwrite
-    and not had_selection
-    and col1 < #self.lines[line1]
-    and text:ulen() == 1 then
-      self:remove(line1, col1, translate.next_char(self, line1, col1))
-    end
-
-    self:insert(line1, col1, text)
-    self:move_to_cursor(sidx, #text)
-  end
-end
-
-function Doc:ime_text_editing(text, start, length, idx)
-  for sidx, line1, col1, line2, col2 in self:get_selections(true, idx or true) do
-    if line1 ~= line2 or col1 ~= col2 then
-      self:delete_to_cursor(sidx)
-    end
-    self:insert(line1, col1, text)
-    self:set_selections(sidx, line1, col1 + #text, line1, col1)
-  end
-end
-
-function Doc:replace_cursor(idx, line1, col1, line2, col2, fn)
-  local old_text = self:get_text(line1, col1, line2, col2)
-  local new_text, res = fn(old_text)
-  if old_text ~= new_text then
-    self:insert(line2, col2, new_text)
-    self:remove(line1, col1, line2, col2)
-    if line1 == line2 and col1 == col2 then
-      line2, col2 = self:position_offset(line1, col1, #new_text)
-      self:set_selections(idx, line1, col1, line2, col2)
-    end
-  end
-  return res
-end
-
-function Doc:replace(fn)
-  local has_selection, results = false, {}
-  for idx, line1, col1, line2, col2 in self:get_selections(true) do
-    if line1 ~= line2 or col1 ~= col2 then
-      results[idx] = self:replace_cursor(idx, line1, col1, line2, col2, fn)
-      has_selection = true
-    end
-  end
-  if not has_selection then
-    self:set_selection(table.unpack(self.selections))
-    results[1] = self:replace_cursor(1, 1, 1, #self.lines, #self.lines[#self.lines], fn)
-  end
-  return results
-end
-
-function Doc:delete_to_cursor(idx, ...)
-  for sidx, line1, col1, line2, col2 in self:get_selections(true, idx) do
-    if line1 ~= line2 or col1 ~= col2 then
-      self:remove(line1, col1, line2, col2)
-    else
-      local l2, c2 = self:position_offset(line1, col1, ...)
-      self:remove(line1, col1, l2, c2)
-      line1, col1 = sort_positions(line1, col1, l2, c2)
-    end
-    self:set_selections(sidx, line1, col1)
-  end
-  self:merge_cursors(idx)
-end
-
-function Doc:delete_to(...) return self:delete_to_cursor(nil, ...) end
-
-function Doc:move_to_cursor(idx, ...)
-  for sidx, line, col in self:get_selections(false, idx) do
-    self:set_selections(sidx, self:position_offset(line, col, ...))
-  end
-  self:merge_cursors(idx)
-end
-
-function Doc:move_to(...) return self:move_to_cursor(nil, ...) end
-
-function Doc:select_to_cursor(idx, ...)
-  for sidx, line, col, line2, col2 in self:get_selections(false, idx) do
-    line, col = self:position_offset(line, col, ...)
-    self:set_selections(sidx, line, col, line2, col2)
-  end
-  self:merge_cursors(idx)
-end
-
-function Doc:select_to(...) return self:select_to_cursor(nil, ...) end
 
 function Doc:get_indent_string()
   local indent_type, indent_size = self:get_indent_info()
@@ -605,58 +334,7 @@ function Doc:get_indent_string()
   return string.rep(" ", indent_size)
 end
 
--- returns the size of the original indent, and the indent
--- in your config format, rounded either up or down
-function Doc:get_line_indent(line, rnd_up)
-  local _, e = line:find("^[ \t]+")
-  local indent_type, indent_size = self:get_indent_info()
-  local soft_tab = string.rep(" ", indent_size)
-  if indent_type == "hard" then
-    local indent = e and line:sub(1, e):gsub(soft_tab, "\t") or ""
-    return e, indent:gsub(" +", rnd_up and "\t" or "")
-  else
-    local indent = e and line:sub(1, e):gsub("\t", soft_tab) or ""
-    local number = #indent / #soft_tab
-    return e, indent:sub(1,
-      (rnd_up and math.ceil(number) or math.floor(number)) * #soft_tab)
-  end
-end
 
--- un/indents text; behaviour varies based on selection and un/indent.
--- * if there's a selection, it will stay static around the
---   text for both indenting and unindenting.
--- * if you are in the beginning whitespace of a line, and are indenting, the
---   cursor will insert the exactly appropriate amount of spaces, and jump the
---   cursor to the beginning of first non whitespace characters
--- * if you are not in the beginning whitespace of a line, and you indent, it
---   inserts the appropriate whitespace, as if you typed them normally.
--- * if you are unindenting, the cursor will jump to the start of the line,
---   and remove the appropriate amount of spaces (or a tab).
-function Doc:indent_text(unindent, line1, col1, line2, col2)
-  local text = self:get_indent_string()
-  local _, se = self.lines[line1]:find("^[ \t]+")
-  local in_beginning_whitespace = col1 == 1 or (se and col1 <= se + 1)
-  local has_selection = line1 ~= line2 or col1 ~= col2
-  if unindent or has_selection or in_beginning_whitespace then
-    local l1d, l2d = #self.lines[line1], #self.lines[line2]
-    for line = line1, line2 do
-      if not has_selection or #self.lines[line] > 1 then -- don't indent empty lines in a selection
-        local e, rnded = self:get_line_indent(self.lines[line], unindent)
-        self:remove(line, 1, line, (e or 0) + 1)
-        self:insert(line, 1,
-          unindent and rnded:sub(1, #rnded - #text) or rnded .. text)
-      end
-    end
-    l1d, l2d = #self.lines[line1] - l1d, #self.lines[line2] - l2d
-    if (unindent or in_beginning_whitespace) and not has_selection then
-      local start_cursor = (se and se + 1 or 1) + l1d or #(self.lines[line1])
-      return line1, start_cursor, line2, start_cursor
-    end
-    return line1, col1 + l1d, line2, col2 + l2d
-  end
-  self:insert(line1, col1, text)
-  return line1, col1 + #text, line1, col1 + #text
-end
 
 -- For plugins to add custom actions of document change
 function Doc:on_text_change(type)
