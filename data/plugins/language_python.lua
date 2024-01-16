be05d27c35e360f587d5b5c5e56de8bdec2bfb1b
Fiji <156847941+FijiHasGithub@users.noreply.github.com>
2024-01-16 20:24:52 +0000

    Improve number highlighting for python syntax highlighting
    Adds support for octal and binary representation, as well uppercase characters (X, B and O)

--- data/plugins/language_python.lua
+++ data/plugins/language_python.lua
@@ -16,7 +16,7 @@ syntax.add {
     { pattern = { "[ruU]?'''", "'''", '\\' },  type = "string"   },
     { pattern = { '[ruU]?"', '"', '\\' },      type = "string"   },
     { pattern = { "[ruU]?'", "'", '\\' },      type = "string"   },
-    { pattern = "0x[%da-fA-F]+",               type = "number"   },
+    { pattern = "0[x|b|o|X|B|O][%da-fA-F]+",   type = "number"   },
     { pattern = "-?%d+[%d%.eE]*",              type = "number"   },
     { pattern = "-?%.?%d+",                    type = "number"   },
     { pattern = "[%+%-=/%*%^%%<>!~|&]",        type = "operator" },
