df2d99d1d862f64a0ad1f1813da67df387be5c4b
Gaspartcho <93390411+Gaspartcho@users.noreply.github.com>
2024-03-27 18:41:53 +0000

    Update language_python.lua
    Dictionaries appear now normally

--- data/plugins/language_python.lua
+++ data/plugins/language_python.lua
@@ -2,85 +2,7 @@
 local syntax = require "core.syntax"
 
 
-local python_fstring = {
-
-    patterns = {
-      { pattern = {"{", "}"},  type = "normal", syntax = ".py" },
-      { pattern = "%g",        type = "string"                 },
-
-    },
-
-    symbols = {}
-}
-
-
-local python_type = {
-
-  patterns = {
-    { pattern = "|",          type = "operator"  },
-    { pattern = "[%w_]+",     type = "keyword2"  },
-    { pattern = "%a%a+",      type = "symbol"    },
-  },
-
-  symbols = {
-    ["None"] = "literal"
-  }
-}
--- Add this line after in order for the recursion to work.
--- Makes sure that the square brackets are well balanced when capturing the syntax 
--- (in order to make something like this work: Tuple[Tuple[int, str], float])
-table.insert(python_type.patterns, 1, { pattern = { "%[", "%]" }, syntax = python_type })
-
--- For things like this_list = other_list[a:b:c]
-local not_python_type = {
-  patterns = {
-    { pattern = "-?0[xboXBO][%da-fA-F_]+",       type = "number"   },
-    { pattern = "-?%d+[%d%.eE_]*",               type = "number"   },
-    { pattern = "-?%.?%d+",                      type = "number"   },
-    { pattern = "[%+%-=/%*%^%%<>!~|&]",          type = "operator" },
-    { pattern = "[%a_][%w_]*%f[(]",              type = "function" },
-  },
-
-  symbols = {
-  }
-}
-
-table.insert(not_python_type.patterns, 1, { pattern = { "%[", "%]" }, syntax = not_python_type })
-
-
-syntax.add {
-  name = "Python",
-  files = { "%.py$", "%.pyw$", "%.rpy$", "%.pyi$" },
-  headers = "^#!.*[ /]python",
-  comment = "#",
-  block_comment = { '"""', '"""' },
-  patterns = {
-    { pattern = "#.*",                           type = "comment"                         },
-    { pattern = { '^%s*"""', '"""' },            type = "comment"                         },
-    { pattern = '[uUrR]%f["]',                   type = "keyword"                         },
-    { pattern = "class%s+()[%a_][%w_]*",         type = { "keyword", "keyword2"}          },
-    { pattern = { "%[", "%]" },                                  syntax = not_python_type },
-    { pattern = "lambda().-:",                   type = { "keyword, normal"}              },
-    { pattern = ":%s*\n",                                                                 },
-    { pattern = { ":%s*", "[,%)%s]"},                             syntax = python_type    },
-    { pattern = { "->()", ":" },                 type = { "operator", "normal" }, syntax = python_type },
-    { pattern = { '[ruU]?"""', '"""'; '\\' },    type = "string"                          },
-    { pattern = { "[ruU]?'''", "'''", '\\' },    type = "string"                          },
-    { pattern = { '[ruU]?"', '"', '\\' },        type = "string"                          },
-    { pattern = { "[ruU]?'", "'", '\\' },        type = "string"                          },
-    { pattern = { 'f"', '"', "\\"},              type = "string", syntax = python_fstring },
-    { pattern = { "f'", "'", "\\"},              type = "string", syntax = python_fstring },
-    { pattern = "-?0[xboXBO][%da-fA-F_]+",       type = "number"                          },
-    { pattern = "-?%d+[%d%.eE_]*",               type = "number"                          },
-    { pattern = "-?%.?%d+",                      type = "number"                          },
-    { pattern = "[%+%-=/%*%^%%<>!~|&]",          type = "operator"                        },
-    { pattern = "[%a_][%w_]*%f[(]",              type = "function"                        },
-
-    { pattern = "[%a_][%w_]*",                   type = "symbol"                          },
-
-
-  },
-  symbols = {
+local python_symbols = {
     ["class"]    = "keyword",
     ["finally"]  = "keyword",
     ["is"]       = "keyword",
@@ -120,4 +42,94 @@ syntax.add {
     ["True"]     = "literal",
     ["False"]    = "literal",
   }
+
+
+local python_fstring = {
+
+    patterns = {
+      { pattern = {"{", "}"},  type = "normal", syntax = ".py" },
+      { pattern = "%g",        type = "string"                 },
+
+    },
+
+    symbols = {}
+}
+
+
+local python_type = {
+
+  patterns = {
+    { pattern = "|",          type = "operator"  },
+    { pattern = "[%w_]+",     type = "keyword2"  },
+    { pattern = "%a%a+",      type = "symbol"    },
+  },
+
+  symbols = {
+    ["None"] = "literal"
+  }
+}
+-- Add this line after in order for the recursion to work.
+-- Makes sure that the square brackets are well balanced when capturing the syntax 
+-- (in order to make something like this work: Tuple[Tuple[int, str], float])
+table.insert(python_type.patterns, 1, { pattern = { "%[", "%]" }, syntax = python_type })
+
+-- For things like this_list = other_list[a:b:c]
+local not_python_type = {
+  patterns = {
+    { pattern = { '[ruU]?"""', '"""'; '\\' },    type = "string"                          },
+    { pattern = { "[ruU]?'''", "'''", '\\' },    type = "string"                          },
+    { pattern = { '[ruU]?"', '"', '\\' },        type = "string"                          },
+    { pattern = { "[ruU]?'", "'", '\\' },        type = "string"                          },
+    { pattern = { 'f"', '"', "\\"},              type = "string", syntax = python_fstring },
+    { pattern = { "f'", "'", "\\"},              type = "string", syntax = python_fstring },
+    { pattern = "-?0[xboXBO][%da-fA-F_]+",       type = "number"                          },
+    { pattern = "-?%d+[%d%.eE_]*",               type = "number"                          },
+    { pattern = "-?%.?%d+",                      type = "number"                          },
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",          type = "operator"                        },
+    { pattern = "[%a_][%w_]*%f[(]",              type = "function"                        },
+
+    { pattern = "%a%a+",                         type = "symbol"                          },
+  },
+
+  symbols = python_symbols
+}
+
+table.insert(not_python_type.patterns, 1, { pattern = { "%[", "%]" }, syntax = not_python_type })
+table.insert(not_python_type.patterns, 1, { pattern = { "{", "}" },   syntax = not_python_type })
+
+
+syntax.add {
+  name = "Python",
+  files = { "%.py$", "%.pyw$", "%.rpy$", "%.pyi$" },
+  headers = "^#!.*[ /]python",
+  comment = "#",
+  block_comment = { '"""', '"""' },
+  patterns = {
+    { pattern = "#.*",                           type = "comment"                         },
+    { pattern = { '^%s*"""', '"""' },            type = "comment"                         },
+    { pattern = '[uUrR]%f["]',                   type = "keyword"                         },
+    { pattern = "class%s+()[%a_][%w_]*",         type = { "keyword", "keyword2"}          },
+    { pattern = { "%[", "%]" },                                  syntax = not_python_type },
+    { pattern = { "{", "}" },                                    syntax = not_python_type },
+    { pattern = "lambda().-:",                   type = { "keyword, normal"}              },
+    { pattern = ":%s*\n",                        type = "normal"                          },
+    { pattern = { ":%s*", "[,%)%s]"},                            syntax = python_type     },
+    { pattern = { "->()", ":" },                 type = { "operator", "normal" }, syntax = python_type },
+    { pattern = { '[ruU]?"""', '"""'; '\\' },    type = "string"                          },
+    { pattern = { "[ruU]?'''", "'''", '\\' },    type = "string"                          },
+    { pattern = { '[ruU]?"', '"', '\\' },        type = "string"                          },
+    { pattern = { "[ruU]?'", "'", '\\' },        type = "string"                          },
+    { pattern = { 'f"', '"', "\\"},              type = "string", syntax = python_fstring },
+    { pattern = { "f'", "'", "\\"},              type = "string", syntax = python_fstring },
+    { pattern = "-?0[xboXBO][%da-fA-F_]+",       type = "number"                          },
+    { pattern = "-?%d+[%d%.eE_]*",               type = "number"                          },
+    { pattern = "-?%.?%d+",                      type = "number"                          },
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",          type = "operator"                        },
+    { pattern = "[%a_][%w_]*%f[(]",              type = "function"                        },
+
+    { pattern = "%a%a+",                         type = "symbol"                          },
+
+
+  },
+  symbols = python_symbols
 }
