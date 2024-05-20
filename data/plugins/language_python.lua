49deedcde6503fdf23f3e99b25535bee39992a61
Gaspartcho <93390411+Gaspartcho@users.noreply.github.com>
2024-05-20 17:27:38 +0000

    Various changes

--- data/plugins/language_python.lua
+++ data/plugins/language_python.lua
@@ -11,6 +11,7 @@ local python_symbols = {
     ["for"]      = "keyword",
     ["lambda"]   = "keyword",
     ["try"]      = "keyword",
+    ["except"]   = "keyword",
     ["def"]      = "keyword",
     ["async"]    = "keyword",
     ["await"]    = "keyword",
@@ -31,7 +32,6 @@ local python_symbols = {
     ["import"]   = "keyword",
     ["pass"]     = "keyword",
     ["break"]    = "keyword",
-    ["except"]   = "keyword",
     ["in"]       = "keyword",
     ["del"]      = "keyword",
     ["raise"]    = "keyword",
@@ -42,19 +42,6 @@ local python_symbols = {
     ["True"]     = "literal",
     ["False"]    = "literal",
   }
-
-
-local python_fstring = {
-
-    patterns = {
-      { pattern = {"{", "}"},  type = "normal", syntax = ".py" },
-      { pattern = "\\.",       type = "string"                 },
-      { pattern = '[^"\\{]+',  type = "string"                 }
-
-    },
-
-    symbols = {}
-}
 
 
 local python_type = {
@@ -70,9 +57,11 @@ local python_type = {
   }
 }
 -- Add this line after in order for the recursion to work.
--- Makes sure that the square brackets are well balanced when capturing the syntax 
+-- Makes sure that the square brackets are well balanced when capturing the syntax
 -- (in order to make something like this work: Tuple[Tuple[int, str], float])
 table.insert(python_type.patterns, 1, { pattern = { "%[", "%]" }, syntax = python_type })
+
+
 
 -- For things like this_list = other_list[a:b:c]
 local not_python_type = {
@@ -81,22 +70,41 @@ local not_python_type = {
     { pattern = { "[ruU]?'''", "'''", '\\' },    type = "string"                          },
     { pattern = { '[ruU]?"', '"', '\\' },        type = "string"                          },
     { pattern = { "[ruU]?'", "'", '\\' },        type = "string"                          },
-    { pattern = { 'f"', '"', "\\"},              type = "string", syntax = python_fstring },
-    { pattern = { "f'", "'", "\\"},              type = "string", syntax = python_fstring },
-    { pattern = "-?0[xboXBO][%da-fA-F_]+",       type = "number"                          },
-    { pattern = "-?%d+[%d%.eE_]*",               type = "number"                          },
-    { pattern = "-?%.?%d+",                      type = "number"                          },
+
+    { pattern = "0[xboXBO][%da-fA-F_]+",         type = "number"                          },
+    { pattern = "%d+[%d%.eE_]*",                 type = "number"                          },
+    { pattern = "%.?%d+",                        type = "number"                          },
+    { pattern = "%f[-%w]-%f[%d]",                type = "number"                          },
+
     { pattern = "[%+%-=/%*%^%%<>!~|&]",          type = "operator"                        },
     { pattern = "[%a_][%w_]*%f[(]",              type = "function"                        },
 
-    { pattern = "%a%a+",                         type = "symbol"                          },
+    { pattern = "%f[%a_]%a%a+%f[^%a_]",          type = "symbol"                          },
   },
 
   symbols = python_symbols
 }
 
 table.insert(not_python_type.patterns, 1, { pattern = { "%[", "%]" }, syntax = not_python_type })
-table.insert(not_python_type.patterns, 1, { pattern = { "{", "}" },   syntax = not_python_type })
+table.insert(not_python_type.patterns, 1, { pattern = { "{",  "}"  }, syntax = not_python_type })
+
+
+
+local python_fstring = {
+
+    patterns = {
+      { pattern = {"{", "}"},    type = "normal", syntax = not_python_type },
+      { pattern = "\\.",         type = "string"                 },
+      { pattern = '[^"\\{}\']+', type = "string"                 }
+
+    },
+
+    symbols = {}
+}
+
+table.insert(not_python_type.patterns, 3, { pattern = { 'f"', '"', "\\"}, type = "string", syntax = python_fstring })
+table.insert(not_python_type.patterns, 3, { pattern = { "f'", "'", "\\"}, type = "string", syntax = python_fstring })
+
 
 
 syntax.add {
@@ -105,32 +113,56 @@ syntax.add {
   headers = "^#!.*[ /]python",
   comment = "#",
   block_comment = { '"""', '"""' },
+
   patterns = {
-    { pattern = "#.*",                           type = "comment"                         },
-    { pattern = { '^%s*"""', '"""' },            type = "comment"                         },
-    { pattern = '[uUrR]%f["]',                   type = "keyword"                         },
-    { pattern = "class%s+()[%a_][%w_]*",         type = { "keyword", "keyword2"}          },
+
+    { pattern = "#.*",                         type = "comment"                           },
+    { pattern = { '^%s*"""', '"""' },          type = "comment"                           },
+    { pattern = '[uUrR]%f["]',                 type = "keyword"                           },
+    { pattern = "class%s+()[%a_][%w_]*():",    type = { "keyword", "keyword2", "normal" } },
+
     { pattern = { "%[", "%]" },                                  syntax = not_python_type },
-    { pattern = { "{", "}" },                                    syntax = not_python_type },
-    { pattern = "lambda().-:",                   type = { "keyword, normal"}              },
-    { pattern = ":%s*\n",                        type = "normal"                          },
-    { pattern = { ":%s*", "%f[=,%)%s]"},                           syntax = python_type     },
-    { pattern = { "->()", ":" },                 type = { "operator", "normal" }, syntax = python_type },
-    { pattern = { '[ruU]?"""', '"""'; '\\' },    type = "string"                          },
-    { pattern = { "[ruU]?'''", "'''", '\\' },    type = "string"                          },
-    { pattern = { '[ruU]?"', '"', '\\' },        type = "string"                          },
-    { pattern = { "[ruU]?'", "'", '\\' },        type = "string"                          },
-    { pattern = { 'f"', '"', "\\"},              type = "string", syntax = python_fstring },
-    { pattern = { "f'", "'", "\\"},              type = "string", syntax = python_fstring },
-    { pattern = "-?0[xboXBO][%da-fA-F_]+",       type = "number"                          },
-    { pattern = "-?%d+[%d%.eE_]*",               type = "number"                          },
-    { pattern = "-?%.?%d+",                      type = "number"                          },
-    { pattern = "[%+%-=/%*%^%%<>!~|&]",          type = "operator"                        },
-    { pattern = "[%a_][%w_]*%f[(]",              type = "function"                        },
+    { pattern = { "{",  "}"  },                                  syntax = not_python_type },
+    { pattern = { "%(", "%)" },                                  syntax = ".py"           },
 
-    { pattern = "%a%a+",                         type = "symbol"                          },
+    { pattern = "lambda().-:",                 type = { "keyword", "normal" }             },
 
+    { pattern = { "def",     "[:\n]" },                          syntax = ".py"           }, -- this and the following prevent one-liner highlight bugs
+
+    { pattern = { "for",     "[:\n]" },                          syntax = not_python_type },
+    { pattern = { "if",      "[:\n]" },                          syntax = not_python_type },
+    { pattern = { "elif",    "[:\n]" },                          syntax = not_python_type },
+    { pattern = { "while",   "[:\n]" },                          syntax = not_python_type },
+    { pattern = { "match",   "[:\n]" },                          syntax = not_python_type },
+    { pattern = { "case",    "[:\n]" },                          syntax = not_python_type },
+    { pattern = { "except",  "[:\n]" },                          syntax = not_python_type },
+
+    { pattern =  "else():",                    type = { "keyword", "normal" }             },
+    { pattern =  "try():",                     type = { "keyword", "normal" }             },
+
+    { pattern = ":%s*\n",                      type = "normal"                            },
+
+    { pattern = { ":%s*", "%f[^%[%]%w_]"},                        syntax = python_type    },
+    { pattern = { "->()", "%f[:]" },           type = { "operator", "normal" }, syntax = python_type },
+
+    { pattern = { '[ruU]?"""', '"""'; '\\' },  type = "string"                            },
+    { pattern = { "[ruU]?'''", "'''", '\\' },  type = "string"                            },
+    { pattern = { '[ruU]?"', '"', '\\' },      type = "string"                            },
+    { pattern = { "[ruU]?'", "'", '\\' },      type = "string"                            },
+    { pattern = { 'f"', '"', "\\"},            type = "string", syntax = python_fstring   },
+    { pattern = { "f'", "'", "\\"},            type = "string", syntax = python_fstring   },
+
+    { pattern = "%f[%w_]0[xboXBO][%da-fA-F_]+%f[^%w_]",         type = "number"           },
+    { pattern = "%f[%w_]%d+[%d%.eE_]*%f[^%w_]",                 type = "number"           },
+    { pattern = "%f[%w_]%.?%d+%f[^%w_]",                        type = "number"           },
+    { pattern = "%f[-%w_]-%f[%d%.]",           type = "number"                            },
+
+    { pattern = "[%+%-=/%*%^%%<>!~|&]",        type = "operator"                          },
+    { pattern = "[%a_][%w_]*%f[(]",            type = "function"                          },
+
+    { pattern = "%f[%w_]%a%a+%f[^%w_]",        type = "symbol"                            },
 
   },
+
   symbols = python_symbols
 }
