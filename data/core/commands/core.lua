7aa7d5808b939031aaa134cd4558bf75ce8e00a8
Adam Harrison <adamdharrison@gmail.com>
2024-06-27 15:57:49 -0400

    (fix): passed missing core.window to set_window_mode call

--- data/core/commands/core.lua
+++ data/core/commands/core.lua
@@ -42,7 +42,7 @@ command.add(nil, {
     if fullscreen then
       restore_title_view = core.title_view.visible
     end
-    system.set_window_mode(fullscreen and "fullscreen" or "normal")
+    system.set_window_mode(core.window, fullscreen and "fullscreen" or "normal")
     core.show_title_bar(not fullscreen and restore_title_view)
     core.title_view:configure_hit_test(not fullscreen and restore_title_view)
   end,
