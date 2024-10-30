06fd57e70dd01cdf044ccf6b97550ba518ad8d2e
Josh Piasecki <138541977+FloppyDisco@users.noreply.github.com>
2024-10-30 11:40:28 +0100

    Update data/plugins/treeview.lua
    Co-authored-by: Takase <20792268+takase1121@users.noreply.github.com>

--- data/plugins/treeview.lua
+++ data/plugins/treeview.lua
@@ -677,7 +677,7 @@ command.add(nil, {
         previous_view = core.root_view:get_primary_node().active_view
       end
       core.set_active_view(view)
-      if(not view.visible)then
+      if not view.visible then
         view.visible = true
       end
       if not view.selected_item then
