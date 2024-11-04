75d31570f9667ba0bca5c585f2c734a8dd5136a8
Adam Harrison <adamdharrison@gmail.com>
2024-11-04 18:27:01 -0500

    Moved things around.

--- scripts/generate-all-in-one.lua
+++ scripts/generate-all-in-one.lua
@@ -5,6 +5,6 @@ aio:write("const char* packaged_files[] = {\n")
 for i, file in ipairs({ ... }) do 
   if i > 1 then aio:write(",\n") end 
   local contents = io.open(file, "rb"):read("*all") 
-  aio:write("\"%INTERNAL%/", file:gsub("data/",""), "\",\"", contents:gsub(".",function(c) return string.format("\\x%02X",string.byte(c)) end), "\", (const char*)", #contents, "LL") 
+  aio:write("\"/", file:gsub("data/",""), "\",\"", contents:gsub(".",function(c) return string.format("\\x%02X",string.byte(c)) end), "\", (const char*)", #contents, "LL") 
 end 
 aio:write("\n, 0, 0, 0\n};");
