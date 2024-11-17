4a51dfdff71ad4ed70ee977e98ff2056262dd82f
takase1121 <20792268+takase1121@users.noreply.github.com>
2024-11-17 16:59:07 +0800

    package-dmg: remove extraneous steps

--- scripts/package-dmg.sh
+++ scripts/package-dmg.sh
@@ -6,12 +6,6 @@ if [ ! -e "src/api/api.h" ]; then
   exit 1
 fi
 
-mkdir -p "Lite XL.app/Contents/MacOS" "Lite XL.app/Contents/Resources"
-cp -r $1/lite-xl/lite-xl "Lite XL.app/Contents/MacOS"
-cp -r $1/lite-xl/data/* "Lite XL.app/Contents/Resources"
-cp resources/icons/icon.icns "Lite XL.app/Contents/Resources"
-cp -r $1/Info.plist "Lite XL.app/Contents/Resources"
-
 dmgbuild -s resources/macos/lite-xl-dmg.py "Lite XL" "$2.dmg"
 
 rm -rf "Lite XL.app"
