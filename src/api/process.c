77a581f43f1f7d83bd89a29b8ed4cfee2df117fd
takase1121 <20792268+takase1121@users.noreply.github.com>
2025-05-24 00:12:55 +0800

    WIP process rewrite

--- src/api/process.c
+++ src/api/process.c
@@ -1,34 +1,29 @@
 #include "api.h"
 
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_thread.h>
 
-#define READ_BUF_SIZE 4096
-#define PROCESS_TERM_TRIES 3
-#define PROCESS_TERM_DELAY 50
+#define PROCESS_READ_BUF_SIZE 4096
 #define PROCESS_KILL_LIST_NAME "__process_kill_list__"
+#define PROCESS_KILL_TRIES 3
+#define PROCESS_KILL_DELAY 10
+#define PROCESS_KILL_LIST_MIN_SIZE 16
+#define PROCESS_PROP_SDL_PTR "lxl.process.SDL_Process.ptr"
+#define PROCESS_PROP_KILL_TRIES_NUM "lxl.process.kill_tries.int"
 
 #if defined(SDL_PLATFORM_UNIX)
 #include <signal.h>
-#if defined(SDL_PLATFORM_WINDOWS)
+#elif defined(SDL_PLATFORM_WINDOWS)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #endif
 
-#ifndef SDL_PROP_PROCESS_CREATE_WORKING_DIRECTORY_STRING
+#if !defined(SDL_PROP_PROCESS_CREATE_WORKING_DIRECTORY_STRING) || !defined(SDL_PROP_PROCESS_CREATE_CMDLINE_STRING)
+#undef SDL_PROP_PROCESS_CREATE_WORKING_DIRECTORY_STRING
+#undef SDL_PROP_PROCESS_CREATE_CMDLINE_STRING
 #define SDL_PROP_PROCESS_CREATE_WORKING_DIRECTORY_STRING "TODO"
-#warning SDL_PROP_PROCESS_CREATE_WORKING_DIRECTORY_STRING not supported
-#endif
-
-#ifndef SDL_PROP_PROCESS_CREATE_CMDLINE_STRING
 #define SDL_PROP_PROCESS_CREATE_CMDLINE_STRING "TODO"
-#warning SDL_PROP_PROCESS_CREATE_CMDLINE_STRING not supported
+#warning SDL_PROP_PROCESS_CREATE_WORKING_DIRECTORY_STRING or SDL_PROP_PROCESS_CREATE_CMDLINE_STRING not supported
 #endif
-
-#define LXL_PROCESS_PROP_PROCESS_PTR "lxl.process.sdl_process.ptr"
-#define LXL_PROCESS_PROP_DEADLINE_NUM "lxl.process.deadline.int"
-#define LXL_PROCESS_PROP_RETURNCODE_NUM "lxl.process.returncode.int"
-#define LXL_PROCESS_PROP_STREAM_CLOSED_NUM "lxl.process.stream_closed.int"
 
 typedef enum {
   SIGNAL_KILL,
@@ -43,7 +38,7 @@ typedef enum {
   REDIRECT_STDOUT
 } redirect_e;
 
-static const char *enum_redirect = {
+static const char *enum_redirect[] = {
   "pipe",
   "parent",
   "discard",
@@ -52,44 +47,113 @@ static const char *enum_redirect = {
 };
 
 typedef enum {
-  STREAM_STDIN = 1 << 0,
-  STREAM_STDOUT = 1 << 1,
-  STREAM_STDERR = 1 << 2,
+  STREAM_STDIN,
+  STREAM_STDOUT,
+  STREAM_STDERR,
 } stream_type_e;
 
-// love bitfields
-static const char *enum_stream_type = { "stdin", "stdout", "stderr", NULL };
-
-static const char *enum_sdl_stream_type[] = {
-  SDL_PROP_PROCESS_STDIN_POINTER,
+static const char *enum_read_type[] = { "stdout", "stderr", NULL };
+static const char *enum_sdl_read_type[] = {
   SDL_PROP_PROCESS_STDOUT_POINTER,
   SDL_PROP_PROCESS_STDERR_POINTER,
   NULL
 };
 
+#if 1 // KILL LIST
+typedef struct {
+  Uint64 max_idx, cap, size, last_swept;
+  SDL_PropertiesID *items;
+} kill_list_t;
+
+static bool kill_list_resize(kill_list_t *list, float ratio) {
+  size_t new_cap = list->cap * ratio;
+  SDL_PropertiesID *new_list = SDL_realloc(list->items, new_cap * sizeof(SDL_PropertiesID));
+  if (!new_list)
+    return false;
+  for (size_t i = list->cap; i < new_cap; i++)
+    SDL_zerop(&new_list[i]);
+  list->items = new_list;
+  list->cap = new_cap;
+  return true;
+}
+
+static bool kill_list_add(kill_list_t *list, SDL_PropertiesID prop) {
+  // attempt to resize list if not sufficient
+  if (list->max_idx >= list->cap && !kill_list_resize(list, 1.5f))
+    return false;
+
+  list->items[list->max_idx++] = prop;
+  list->size++;
+  return true;
+}
+
+static void kill_list_sweep(kill_list_t *list, bool force) {
+  if (SDL_GetTicks() - list->last_swept < PROCESS_KILL_DELAY) return;
+  
+  // sweep the list, removing processes when they die
+  size_t last_idx = 0;
+  for (size_t i = 0; i < list->max_idx; i++) {
+    if (list->items[i] == 0) break;
+
+    SDL_Process *proc = SDL_GetPointerProperty(list->items[i], PROCESS_PROP_SDL_PTR, NULL);
+    SDL_assert(proc != NULL);
+
+    int tries = SDL_GetNumberProperty(list->items[i], PROCESS_PROP_KILL_TRIES_NUM, 0);
+    SDL_KillProcess(proc, tries > PROCESS_KILL_TRIES);
+    SDL_SetNumberProperty(list->items[i], PROCESS_PROP_KILL_TRIES_NUM, ++tries);
+
+    // if process died or exceeded the amount of tries we can do, just destroy it
+    if ((list->items[i] != 0 && tries > PROCESS_KILL_TRIES + 1) || !SDL_WaitProcess(proc, false, NULL)) {
+      SDL_DestroyProperties(list->items[i]);
+      list->size--;
+      if (i < list->max_idx)
+        list->items[i] = list->items[i+1];
+      else
+        SDL_zerop(&list->items[i]);
+    }
+    
+    if (list->items[i] != 0)
+      last_idx = i;
+  }
+  list->max_idx = last_idx + 1;
+
+  // attempt to shrink if utilisation is less than 50% and we just compacted (so everything is on the left)
+  if (list->cap > PROCESS_KILL_LIST_MIN_SIZE && ((double) list->max_idx / list->cap) < 0.5)
+    kill_list_resize(list, 0.7f);
+
+  list->last_swept = SDL_GetTicks();
+}
+#endif
+
+#if 1 // CLEANUP
 static int f_cleanup_properties(lua_State *L) {
-  SDL_PropertiesID id = lua_rawgeti(L, 1, 1);
+  SDL_PropertiesID id = (lua_rawgeti(L, 1, 1), lua_tointeger(L, -1));
   if (id != 0) SDL_DestroyProperties(id);
   return 0;
 }
 
-static void free_null_list(void *ud, void *ptr) {
+static void destroy_null_array(void *ud, void *ptr) {
   (void) ud;
   for (void **p = ptr; *p; p++) SDL_free(*p);
   SDL_free(ptr);
 }
 
-static void free_sdl_env(void *ud, void *ptr) {
+static void destroy_SDL_Environment(void *ud, void *ptr) {
   (void) ud;
   if (ptr) SDL_DestroyEnvironment((SDL_Environment *) ptr);
 }
 
+static void destroy_SDL_Process(void *ud, void *ptr) {
+  (void) ud;
+  if (ptr) SDL_DestroyProcess((SDL_Process *) ptr);
+}
+#endif
+
 #define GET_PROCESS(L, idx) \
-  SDL_GetPointerProperty(*((SDL_PropertiesID *) luaL_checkudata(L, 1, API_TYPE_PROCESS)), LXL_PROCESS_PROP_PROCESS_PTR, NULL)
+  SDL_GetPointerProperty(*((SDL_PropertiesID *) luaL_checkudata(L, 1, API_TYPE_PROCESS)), PROCESS_PROP_SDL_PTR, NULL)
 
 #define GET_PROCESS_PROP(L, idx) \
   *((SDL_PropertiesID *) luaL_checkudata(L, 1, API_TYPE_PROCESS))
-
 
 static int process_start(lua_State* L) {
   lua_settop(L, 2);
@@ -100,7 +164,7 @@ static int process_start(lua_State* L) {
 
   SDL_PropertiesID props = SDL_CreateProperties();
   if (!props)
-    return luaL_error("cannot start process: %s", SDL_GetError());
+    return luaL_error(L, "cannot start process: %s", SDL_GetError());
   
   // create properties on a to be closed slot
   lua_newtable(L);
@@ -118,7 +182,7 @@ static int process_start(lua_State* L) {
     char **cmd = SDL_calloc(len + 1, sizeof(char *));
     if (!cmd)
       return luaL_error(L, "cannot start process: cannot allocate memory");
-    SDL_SetPointerPropertyWithCleanup(props, SDL_PROP_PROCESS_CREATE_ARGS_POINTER, cmd, free_null_list, NULL);
+    SDL_SetPointerPropertyWithCleanup(props, SDL_PROP_PROCESS_CREATE_ARGS_POINTER, cmd, destroy_null_array, NULL);
 
     // if this block errors everything is handled by property cleanup
     for (int i = 1; i <= len; i++) {
@@ -173,7 +237,7 @@ static int process_start(lua_State* L) {
   SDL_Environment *env = SDL_CreateEnvironment(true);
   if (!env)
     return luaL_error(L, "cannot start process: %s", SDL_GetError());
-  SDL_SetPointerPropertyWithCleanup(props, SDL_PROP_PROCESS_CREATE_ENVIRONMENT_POINTER, env, free_sdl_env, NULL);
+  SDL_SetPointerPropertyWithCleanup(props, SDL_PROP_PROCESS_CREATE_ENVIRONMENT_POINTER, env, destroy_SDL_Environment, NULL);
 
   // again, environment is cleaned up if this fails
   lua_pushnil(L);
@@ -186,8 +250,8 @@ static int process_start(lua_State* L) {
   SDL_Process *proc = SDL_CreateProcessWithProperties(props);
   if (!proc)
     return luaL_error(L, "cannot start process: %s", SDL_GetError());
+  SDL_SetPointerPropertyWithCleanup(props, PROCESS_PROP_SDL_PTR, proc, destroy_SDL_Process, NULL);
 
-  SDL_SetPointerProperty(props, LXL_PROCESS_PROP_PROCESS_PTR, proc);
   // clear the to-be-closed slot to prevent it from destroying the process
   lua_pushinteger(L, 0); lua_rawseti(L, to_close, 1);
 
@@ -197,19 +261,15 @@ static int process_start(lua_State* L) {
   return 1;
 }
 
-static int g_read(lua_State* L, stream_type_e stream, size_t read_size) {
+static int g_read(lua_State* L, stream_type_e stream_type, size_t read_size) {
   SDL_Process *proc = GET_PROCESS(L, 1);
   SDL_assert(proc != NULL);
-
-  int closed = SDL_GetNumberProperty(GET_PROCESS_PROP(L, 1), LXL_PROCESS_PROP_STREAM_CLOSED_NUM, 0);
-  if (closed & stream)
-    return luaL_error(L, "cannot read stream: stream is closed");
 
   SDL_PropertiesID props = SDL_GetProcessProperties(proc);
   if (!props)
     return luaL_error(L, "cannot read stream: %s", SDL_GetError());
 
-  SDL_ProcessIO *stream = SDL_GetPointerProperty(props, stream == STREAM_STDOUT ? SDL_PROP_PROCESS_STDOUT_POINTER : SDL_PROP_PROCESS_STDERR_POINTER, NULL);
+  SDL_IOStream *stream = SDL_GetPointerProperty(props, enum_sdl_read_type[stream_type], NULL);
   if (!stream)
     return luaL_error(L, "cannot read stream: %s", SDL_GetError());
 
@@ -225,6 +285,7 @@ static int g_read(lua_State* L, stream_type_e stream, size_t read_size) {
     case SDL_IO_STATUS_NOT_READY: lua_pushliteral(L, "wait");  break;
     case SDL_IO_STATUS_READONLY:
     case SDL_IO_STATUS_WRITEONLY: SDL_assert(true && "unreachable code"); break;
+    case SDL_IO_STATUS_READY:     break;
   }
 
   return lua_gettop(L) - top + 1;
@@ -234,11 +295,7 @@ static int f_write(lua_State* L) {
   SDL_Process *proc = GET_PROCESS(L, 1);
   SDL_assert(proc != NULL);
 
-  int closed = SDL_GetNumberProperty(GET_PROCESS_PROP(L, 1), LXL_PROCESS_PROP_STREAM_CLOSED_NUM, 0);
-  if (closed & STREAM_STDIN)
-    return luaL_error(L, "cannot write to stream: stream is closed");
-
-  SDL_ProcessIO *stream = SDL_GetProcessInput(proc);
+  SDL_IOStream *stream = SDL_GetProcessInput(proc);
   if (!stream)
     return luaL_error(L, "cannot write to stream: %s", SDL_GetError());
 
@@ -250,29 +307,10 @@ static int f_write(lua_State* L) {
     case SDL_IO_STATUS_NOT_READY: lua_pushliteral(L, "wait");  break;
     case SDL_IO_STATUS_READONLY:
     case SDL_IO_STATUS_WRITEONLY: SDL_assert(true && "unreachable code"); break;
+    case SDL_IO_STATUS_READY:     break;
   }
 
   return lua_gettop(L) - top;
-}
-
-static int f_close_stream(lua_State* L) {
-  lua_settop(L, 2);
-  SDL_Process *proc = GET_PROCESS(L, 1);
-  SDL_assert(proc != NULL);
-
-  SDL_PropertiesID props = SDL_GetProcessProperties(proc);
-  if (!props)
-    return luaL_error(L, "cannot close stream: %s", SDL_GetError());
-
-  SDL_IOStream *stream = SDL_GetPointerProperty(props, enum_sdl_stream_type[luaL_checkoption(L, 2, NULL, enum_stream_type)], NULL);
-  if (!stream)
-    return luaL_error(L, "cannot close stream: %s", SDL_GetError());
-
-  lua_pushboolean(L, SDL_CloseIO(stream));
-  if (!lua_toboolean(L, -1))
-    lua_pushstring(L, SDL_GetError());
-
-  return lua_gettop(L) - 2;
 }
 
 static int f_tostring(lua_State* L) {
@@ -292,33 +330,32 @@ static int f_pid(lua_State* L) {
   return 1;
 }
 
-static int f_returncode(lua_State *L) {
+static int f_wait(lua_State* L) {
   SDL_Process *proc = GET_PROCESS(L, 1);
   SDL_assert(proc != NULL);
-
   int exitcode = 0;
-  if (!SDL_WaitProcess(proc, false, &exitcode))
-    return luaL_error(L, "cannot get return code: %s", SDL_GetError());
 
-  // god knows this will work since this is not explicitly worked around on windows
+  if (SDL_WaitProcess(proc, false, &exitcode))
+    return 0;
+  
   lua_pushinteger(L, exitcode);
   return 1;
 }
 
+static int f_returncode(lua_State *L) {
+  return f_wait(L);
+}
+
 static int f_read_stdout(lua_State* L) {
-  return g_read(L, STREAM_STDOUT, luaL_optinteger(L, 2, READ_BUF_SIZE));
+  return g_read(L, STREAM_STDOUT, luaL_optinteger(L, 2, PROCESS_READ_BUF_SIZE));
 }
 
 static int f_read_stderr(lua_State* L) {
-  return g_read(L, STREAM_STDERR, luaL_optinteger(L, 2, READ_BUF_SIZE));
+  return g_read(L, STREAM_STDERR, luaL_optinteger(L, 2, PROCESS_READ_BUF_SIZE));
 }
 
 static int f_read(lua_State* L) {
-  return g_read(L, luaL_checkoption(L, 2, NULL, enum_stream_type), luaL_optinteger(L, 3, READ_BUF_SIZE));
-}
-
-static int f_wait(lua_State* L) {
-  return f_returncode(L);
+  return g_read(L, luaL_checkoption(L, 2, NULL, enum_read_type), luaL_optinteger(L, 3, PROCESS_READ_BUF_SIZE));
 }
 
 static int g_signal(lua_State* L, signal_e sig) {
@@ -336,70 +373,71 @@ static int g_signal(lua_State* L, signal_e sig) {
       int pid = SDL_GetNumberProperty(props, SDL_PROP_PROCESS_PID_NUMBER, 0);
       if (pid == 0)
         return luaL_error(L, "cannot kill process: cannot get PID");
-      kill(-pid);
+      kill(-pid, SIGINT);
 #elif defined(SDL_PLATFORM_WINDOWS)
-
+      SDL_PropertiesID props = SDL_GetProcessProperties(proc);
+      if (!props)
+        return luaL_error(L, "cannot kill process: %s", SDL_GetError());
+      int pid = SDL_GetNumberProperty(props, SDL_PROP_PROCESS_PID_NUMBER, 0);
+      HANDLE proc_handle = OpenProcess(PROCESS_SUSPEND_RESUME, FALSE, pid);
+      if (!proc_handle)
+        return luaL_error(L, "cannot kill process: OpenProcess() returned %d", GetLastError());
+      DebugBreakProcess(proc_handle);
+      CloseHandle(proc_handle);
 #endif
+      break;
     }
   }
-  process_t* self = (process_t*) luaL_checkudata(L, 1, API_TYPE_PROCESS);
-  signal_process(self, sig);
   lua_pushboolean(L, 1);
   return 1;
 }
+
 static int f_terminate(lua_State* L) { return g_signal(L, SIGNAL_TERM); }
 static int f_kill(lua_State* L) { return g_signal(L, SIGNAL_KILL); }
 static int f_interrupt(lua_State* L) { return g_signal(L, SIGNAL_INTERRUPT); }
 
 static int f_gc(lua_State* L) {
-  process_kill_list_t *list = NULL;
-  process_kill_t *p = NULL;
-  process_t* self = (process_t*) luaL_checkudata(L, 1, API_TYPE_PROCESS);
+  SDL_Process *proc = GET_PROCESS(L, 1);
+  SDL_assert(proc != NULL);
 
-  // get the kill_list for the lua_State
-  if (lua_getfield(L, LUA_REGISTRYINDEX, PROCESS_KILL_LIST_NAME) == LUA_TUSERDATA)
-    list = (process_kill_list_t *) lua_touserdata(L, -1);
+  SDL_KillProcess(proc, false);
+  if (!SDL_WaitProcess(proc, false, NULL))
+    goto destroy;
 
-  if (poll_process(self, 0) && !self->detached) {
-    // attempt to kill the process if still running and not detached
-    signal_process(self, SIGNAL_TERM);
-    if (!list || !list->worker_thread || !(p = SDL_malloc(sizeof(process_kill_t)))) {
-      // use synchronous waiting
-      if (poll_process(self, PROCESS_TERM_DELAY)) {
-        signal_process(self, SIGNAL_KILL);
-        poll_process(self, PROCESS_TERM_DELAY);
-      }
-    } else {
-      // put the handle into a queue for asynchronous waiting
-      p->handle = PROCESS_GET_HANDLE(self);
-      p->start_time = SDL_GetTicks();
-      p->tries = 1;
-      SDL_LockMutex(list->mutex);
-      kill_list_push(list, p);
-      SDL_SignalCondition(list->has_work);
-      SDL_UnlockMutex(list->mutex);
-    }
-  }
-  close_fd(&self->child_pipes[STDIN_FD ][1]);
-  close_fd(&self->child_pipes[STDOUT_FD][0]);
-  close_fd(&self->child_pipes[STDERR_FD][0]);
+  // add to kill list; if it doesn't exist, fall back to killing them synchronously
+  kill_list_t *list = (lua_getfield(L, LUA_REGISTRYINDEX, PROCESS_KILL_LIST_NAME), lua_touserdata(L, -1));
+  if (!list || kill_list_add(list, GET_PROCESS_PROP(L, 1)))
+    goto fallback;
+
+  return 0;
+
+fallback:
+  SDL_KillProcess(proc, true);
+  SDL_WaitProcess(proc, true, NULL);
+destroy:
+  SDL_DestroyProperties(GET_PROCESS_PROP(L, 1));
   return 0;
 }
 
 static int f_running(lua_State* L) {
-  process_t* self = (process_t*)luaL_checkudata(L, 1, API_TYPE_PROCESS);
-  lua_pushboolean(L, poll_process(self, WAIT_NONE));
+  SDL_Process *proc = GET_PROCESS(L, 1);
+  SDL_assert(proc != NULL);
+
+  lua_pushboolean(L, SDL_WaitProcess(proc, false, NULL));
   return 1;
 }
 
 static int process_gc(lua_State *L) {
-  process_kill_list_t *list = NULL;
-  // get the kill_list for the lua_State
-  if (lua_getfield(L, LUA_REGISTRYINDEX, PROCESS_KILL_LIST_NAME) == LUA_TUSERDATA) {
-    list = (process_kill_list_t *) lua_touserdata(L, -1);
-    kill_list_wait_all(list);
-    kill_list_free(list);
+  kill_list_t *list = (lua_getfield(L, LUA_REGISTRYINDEX, PROCESS_KILL_LIST_NAME), lua_touserdata(L, -1));
+  if (!list)
+    return 0;
+  
+  // sweep until this list is empty
+  while (list->size) {
+    kill_list_sweep(list, true);
+    SDL_Delay(PROCESS_KILL_DELAY * 1.1f);
   }
+  SDL_free(list->items);
   return 0;
 }
 
@@ -412,7 +450,6 @@ static const struct luaL_Reg process_metatable[] = {
   {"read_stdout", f_read_stdout},
   {"read_stderr", f_read_stderr},
   {"write", f_write},
-  {"close_stream", f_close_stream},
   {"wait", f_wait},
   {"terminate", f_terminate},
   {"kill", f_kill},
@@ -423,16 +460,17 @@ static const struct luaL_Reg process_metatable[] = {
 
 static const struct luaL_Reg lib[] = {
   { "start", process_start },
-  { "strerror", process_strerror },
   { NULL, NULL }
 };
 
 int luaopen_process(lua_State *L) {
-  process_kill_list_t *list = lua_newuserdata(L, sizeof(process_kill_list_t));
-  if (kill_list_init(list))
-    lua_setfield(L, LUA_REGISTRYINDEX, PROCESS_KILL_LIST_NAME);
+  kill_list_t *list = lua_newuserdata(L, sizeof(kill_list_t));
+  SDL_zerop(list);
+  list->items = SDL_calloc(16, sizeof(SDL_PropertiesID));
+  if (!list->items)
+    lua_pop(L, 1);
   else
-    lua_pop(L, 1); // discard the list
+    lua_setfield(L, LUA_REGISTRYINDEX, PROCESS_KILL_LIST_NAME);
 
   // create the process metatable
   luaL_newmetatable(L, API_TYPE_PROCESS);
@@ -446,15 +484,6 @@ int luaopen_process(lua_State *L) {
   lua_pushcfunction(L, process_gc);
   lua_setfield(L, -2, "__gc");
   lua_setmetatable(L, -2);
-
-  API_CONSTANT_DEFINE(L, -1, "WAIT_INFINITE", WAIT_INFINITE);
-  API_CONSTANT_DEFINE(L, -1, "WAIT_DEADLINE", WAIT_DEADLINE);
-
-  API_CONSTANT_DEFINE(L, -1, "REDIRECT_DEFAULT", REDIRECT_DEFAULT);
-  API_CONSTANT_DEFINE(L, -1, "REDIRECT_STDOUT", STDOUT_FD);
-  API_CONSTANT_DEFINE(L, -1, "REDIRECT_STDERR", STDERR_FD);
-  API_CONSTANT_DEFINE(L, -1, "REDIRECT_PARENT", REDIRECT_PARENT); // Redirects to parent's STDOUT/STDERR
-  API_CONSTANT_DEFINE(L, -1, "REDIRECT_DISCARD", REDIRECT_DISCARD); // Closes the filehandle, discarding it.
 
   return 1;
 }
