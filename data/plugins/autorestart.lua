0077790f8a9ba12c78e6c35b13ed305a98a154ba
Adam <adamdharrison@gmail.com>
2025-08-29 11:25:45 -0400

    Rearrange Plugin Loading and SDL Initialization (#1881)
    * Revert "Removed storage module."
    
    This reverts commit 0bb8d7ec052f2d6412b7ee3459dfb75a694f7ad0.
    
    Revert "Typo."
    
    This reverts commit 283b0ae49274788081e6bf447d5fdb55bc0cb6f1.
    
    Initial commit.
    
    Moved around plugin initialization.
    
    Fixing restarting when changing projects.
    
    Added in autorestart plugin.
    
    Moved screensaver call to after video subsystem init.
    
    Fixed minor issue.
    
    Missing extension.
    
    Kept initailization of previous_find and previous_replace.
    
    Allowed priority regex to take a decimal place.
    
    Fixed priority regex.
    
    Fixed version mismatch error.
    
    Removed hints.
    
    Whoops.
    
    Reverting things.
    
    Fixed up warnings.
    
    * Removed if statements as we're on SDL3.
    
    * updated to SDL3.
    
    * Changed initialization styles to SDL3.
    
    * Removed unecessary dofile.
    
    * Ensured that we can always read files, even binary ones.

---
+++ data/plugins/autorestart.lua
@@ -0,0 +1,19 @@
+-- mod-version:4
+local core = require "core"
+local config = require "core.config"
+local command = require "core.command"
+local Doc = require "core.doc"
+local common = require "core.common"
+
+config.plugins.autorestart = common.merge({
+  
+}, config.plugins.autorestart)
+
+local save = Doc.save
+Doc.save = function(self, ...)
+  local res = save(self, ...)
+  if self.abs_filename == USERDIR .. PATHSEP .. "init.lua" or self.abs_filename == core.root_project().path .. PATHSEP .. ".lite_project" then
+    command.perform("core:restart")
+  end
+  return res
+end
