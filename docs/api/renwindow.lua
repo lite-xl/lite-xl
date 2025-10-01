7828c83800b9cb944d609ae7f94ce98f42c45113
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2025-10-01 23:49:40 +0530

    Update renwindow.lua

--- docs/api/renwindow.lua
+++ docs/api/renwindow.lua
@@ -10,19 +10,26 @@ renwindow = {}
 ---
 --- 
 ---
+---@param str string? if nil will be undefined
 ---@param x integer? if nil will be undefined
 ---@param y integer? if nil will be undefined
 ---@param width integer? if nil or less than 1 will be calculated from display
 ---@param height integer? if nil or less than 1 will be calculated from display
 ---
 ---@return renwindow
-function renwindow.create(x, y, width, height) end
+function renwindow.create(str, x, y, width, height) end
 
 ---
 --- Get width and height of a window 
 ---
----@param renwindow
+---@param window renwindow
 ---
 ---@return number width
 ---@return number height
 function renwindow.get_size(window) end
+
+---
+--- Restore Window
+---
+---@return number
+function renwindow._restore() end
